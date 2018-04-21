8a31324c01929f3fc47a23d42d2a770d57897f8a
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-04-21 18:25:00 +0200

    added protoypes for class Server

---
+++ src/main/java/it/polimi/se2018/Server.java
@@ -0,0 +1,34 @@
+package it.polimi.se2018;
+import java.util.ArrayList;
+
+public class Server {
+
+
+        private int port;
+        private int defaultMachmakinTimer;
+        private int defaultMoveTimer;
+        private String configurationPath;
+        private ArrayList<Player> onlinePlayers = new ArrayList<Player>();
+
+        public Server(){
+
+        }
+
+        private void  loadConfiguration(String path){
+
+        }
+
+        public int getDefaultMachmakinTimer(){
+
+        }
+
+        public int getDefaultMoveTimer(){
+
+        }
+
+        public static void main(String[] args) {
+            // Prints "Hello, World" to the terminal window.
+            System.out.println("Hello, World");
+        }
+
+}
