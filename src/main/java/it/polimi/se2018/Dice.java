89fef49a4cdbed65f0b74df23ea88ec835f2d2f2
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-04-22 13:18:50 +0200

    added protoypes for class Dice

---
+++ src/main/java/it/polimi/se2018/Dice.java
@@ -0,0 +1,38 @@
+package it.polimi.se2018;
+
+public class Dice {
+
+    private int id;
+    private int number;
+    private String color;
+    private String location;
+
+    public Dice(){
+
+    }
+
+    public String getColor() {
+
+    }
+
+    public int getNuber() {
+
+    }
+
+    public String getLocation() {
+
+    }
+
+    public int getId() {
+
+    }
+
+    public void setNumber(int number) {
+
+    }
+
+    public void setLocation(String location) {
+
+    }
+}
+
