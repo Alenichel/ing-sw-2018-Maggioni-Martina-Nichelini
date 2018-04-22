ad5d0a68e362629775c33316873fe637c11d1063
Stefano Martina <stefano.martina@mail.polimi.it>
2018-04-22 12:15:17 +0200

    Added prototype for classe WindowPatternCard

---
+++ src/main/java/it/polimi/se2018/WindowPatternCard.java
@@ -0,0 +1,20 @@
+package it.polimi.se2018;
+
+import java.util.ArrayList;
+
+public class WindowPatternCard extends Card{
+    private ArrayList<WindowCell> composition;
+    private int numberOfRows = 4;
+    private int numberOfColumns = 5;
+    private Game gameReference;
+    private Player player;
+
+    public void windowPatternCard (Game game, Player player){
+
+    }
+
+    public WindowCell getCell(int row, int column){
+        
+    }
+
+}
