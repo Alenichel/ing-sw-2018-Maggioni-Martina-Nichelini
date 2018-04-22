48c924bd7da1fcc31f7d598a8c14e333e37502b9
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-04-22 13:21:20 +0200

    added protoypes for class WindowCell

---
+++ src/main/java/it/polimi/se2018/WindowCell.java
@@ -0,0 +1,31 @@
+package it.polimi.se2018;
+
+public class WindowCell {
+
+    private int row;
+    private int column;
+    private String colorConstraint;
+    private String numberContstraint;
+    private Dice assignedDice;
+
+    public WindowCell() {
+
+    }
+
+    public String getColorConstraint() {
+
+    }
+
+    public int getRow() {
+
+    }
+
+    public int getColumn() {
+
+    }
+
+    public int getNumberConstraint() {
+
+    }
+}
+
