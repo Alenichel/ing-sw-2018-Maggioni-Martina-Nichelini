3a3d0ce2b91915619167682e19a1057f7f3f8905
Stefano Martina <stefano.martina@mail.polimi.it>
2018-04-22 12:09:09 +0200

    Added prototype for classes ObjectiveCard

---
+++ src/main/java/it/polimi/se2018/ObjectiveCard.java
@@ -0,0 +1,15 @@
+package it.polimi.se2018;
+
+public class ObjectiveCard extends Card{
+    private String description;
+    private boolean isPrivate;
+    private Game gameReference;
+
+    public void objectiveCard (Game game){
+        
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
