37a7ef4cb598295210834c0d0a87ecb14511f5b1
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-04-22 13:20:32 +0200

    added protoypes for class Game

---
+++ src/main/java/it/polimi/se2018/Game.java
@@ -0,0 +1,33 @@
+package it.polimi.se2018;
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<Dice> bag = new ArrayLst<Dice>();
+    private ArrayList<WindowPatternCard> patternCards = new ArrayList<WindowPatterCard>();
+    private ArrayList<ObjectiveCards> objectiveCards = new ArrayList<ObjectiveCards>();
+    private ArrayList<ToolCard> toolCards = new ArrayList<ToolCard>();
+    private ArrayList<Player> players = new ArrayList<Player>();
+
+    public Game() {
+
+    }
+
+    public void nextRound() {
+
+    }
+
+    public String showStats() {
+
+    }
+
+    public void givePatternCards() {
+
+    }
+
+    public void initializePatternCards() {
+
+    }
+}
+
+
