a1c69824971798ec428c175335e2e61c5f44a1b4
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-04-22 16:13:13 +0200

    Added configuration setup from file

--- src/main/java/it/polimi/se2018/Server.java
+++ src/main/java/it/polimi/se2018/Server.java
@@ -1,34 +1,69 @@
 package it.polimi.se2018;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Server {
 
 
         private int port;
-        private int defaultMachmakinTimer;
+        private int defaultMatchmakingTimer;
         private int defaultMoveTimer;
-        private String configurationPath;
-        private ArrayList<Player> onlinePlayers = new ArrayList<Player>();
 
-        public Server(){
+        private static final String home_path = System.getProperty("user.home");
+        private static final String configurationFileName = "/sagrada_server_conf.xml";
 
+        //private ArrayList<Player> onlinePlayers = new ArrayList<Player>();
+
+        private static Server instance = null;
+
+
+        protected Server(){
+            try {
+                loadConfiguration();
+            } catch (FileNotFoundException e){
+                System.out.println("[*] Configuration file not found in " + home_path + configurationFileName + "\n[*] Aborting..");
+                System.exit(1);
+            } catch (IOException | SAXException | ParserConfigurationException e) {
+                e.printStackTrace();
+            }
         }
 
-        private void  loadConfiguration(String path){
-
+        public static Server getInstance(){
+            if(instance == null){
+                instance = new Server();
+            }
+            return instance;
         }
 
-        public int getDefaultMachmakinTimer(){
+        private void loadConfiguration() throws ParserConfigurationException, IOException, SAXException, FileNotFoundException {
+                File configurationFile = new File(home_path + configurationFileName);
+                DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder documentBuilder = docBuilderFactory.newDocumentBuilder();
+                Document doc = documentBuilder.parse(configurationFile);
 
+                this.port = Integer.parseInt(doc.getElementsByTagName("port").item(0).getTextContent());
+                this.defaultMatchmakingTimer = Integer.parseInt(doc.getElementsByTagName("defaultMatchmakinTimer").item(0).getTextContent());
+                this.defaultMoveTimer = Integer.parseInt(doc.getElementsByTagName("defaultMoveTimer").item(0).getTextContent());
+        }
+
+        public int getDefaultMatchmakingTimer(){
+                return this.defaultMatchmakingTimer;
         }
 
         public int getDefaultMoveTimer(){
-
+                return this.defaultMoveTimer;
         }
 
-        public static void main(String[] args) {
-            // Prints "Hello, World" to the terminal window.
-            System.out.println("Hello, World");
+        public int getServerPort() {
+            return this.port;
         }
 
 }
