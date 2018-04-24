bf990128f4e084aee640fd49b8d43c7ddc7a98c1
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-04-24 13:27:53 +0200

    fixed get methods for class WindowCell

--- src/main/java/it/polimi/se2018/WindowCell.java
+++ src/main/java/it/polimi/se2018/WindowCell.java
@@ -13,19 +13,19 @@ public class WindowCell {
     }
 
     public String getColorConstraint() {
-        return getColorConstraint();
+        return colorConstraint;
     }
 
     public int getRow() {
-        return getRow();
+        return row;
     }
 
     public int getColumn() {
-        return getColumn();
+        return column;
     }
 
     public int getNumberConstraint() {
-        return getNumberConstraint();
+        return numberConstraint;
     }
 }
 
