a5011a1584d710e1845a157a45abb29b562cbad6
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-04-24 11:42:30 +0200

    Added constructor

--- src/main/java/it/polimi/se2018/GameHandler.java
+++ src/main/java/it/polimi/se2018/GameHandler.java
@@ -1,26 +1,38 @@
 package it.polimi.se2018;
+import java.util.ArrayList;
 
 public class GameHandler {
-    private int id;
+    private int gameId;
     private String gameName;
-    private boolean singlePlayerGame;
+    private boolean singlePlayerMode;
     private boolean started;
-    private int numberOfPlayers = 4;
+    private int numberOfPlayers;
     private int numberOfConnectedPlayer;
     private ArrayList <Player> listOfConnectedPlayer;
-    private final int  numberOfRounc = 10;
+    private static final int  numberOfRound = 10;
     private int currentRound;
 
-    public void gameSetter(){
+    public GameHandler(String name, Player admin, Boolean singlePlayer){
+        this.gameId = this.hashCode();
+        this.gameName = name;
+        this.singlePlayerMode = singlePlayer;
+        this.started = false;
 
+        this.numberOfPlayers = singlePlayer ? 1 : 4;
+        this.numberOfConnectedPlayer = 1;
+        listOfConnectedPlayer.add(admin);
+    }
+
+    public void gameSetter(){
+        //for now empty
     }
 
     public void connectPlayer(Player player){
-
+        //for now empty
     }
 
     public void disconnectPlayer(Player player){
-
+        //for now empty
     }
 
 }
