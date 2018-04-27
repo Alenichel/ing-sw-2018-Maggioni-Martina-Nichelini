b15a61360f38fd3a125da880ecd13ab30331e28a
Stefano Martina <stefano.martina@mail.polimi.it>
2018-04-27 10:39:33 +0200

    Added classes PrivateObjectiveCard and PublicObjectiveCard with prototypes

---
+++ src/main/java/it/polimi/se2018/PrivateObjectiveCard.java
@@ -0,0 +1,8 @@
+package it.polimi.se2018;
+
+public class PrivateObjectiveCard extends ObjectiveCard {
+    
+    public int scorePoint(){
+
+    }
+}
---
+++ src/main/java/it/polimi/se2018/PublicObjectiveCard.java
@@ -0,0 +1,8 @@
+package it.polimi.se2018;
+
+public class PublicObjectiveCard extends ObjectiveCard{
+
+    public int scorePoint(){
+
+    }
+}
