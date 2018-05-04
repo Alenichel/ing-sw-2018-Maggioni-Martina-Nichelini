f60b6ad0b68e23ec35edb069a3303589b4b635a7
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-05-04 19:57:47 +0200

    Implemented ServerController Singleton

--- src/main/java/it/polimi/se2018/ServerController.java
+++ src/main/java/it/polimi/se2018/ServerController.java
@@ -1,16 +1,30 @@
 package it.polimi.se2018;
 
 public class ServerController {
-    private static ServerController instance = new ServerController();
+    private final static ServerController instance = new ServerController();
 
     public static ServerController getInstance() {
         return instance;
     }
 
-    private ServerController() { }
+    private ServerController() {
+    }
 
-    public void connectPlayer (Player player) {}
-    public void disconnectPlayer (Player player) {}
-    public void createRoom (Room room) {}
-    public void deleteRoom (Room room) {}
+    public void connectPlayer (Player player) {
+        instance.connectPlayer(player);
+    }
+
+    public void disconnectPlayer (Player player) {
+        instance.disconnectPlayer(player);
+    }
+
+
+    public void createRoom(Room room) {
+        instance.createRoom(room);
+    }
+
+    public void deleteRoom(Room room) {
+        instance.deleteRoom(room);
+    }
+
 }
