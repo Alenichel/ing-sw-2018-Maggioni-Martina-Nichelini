a1842239b2a515b2efcd86926483f665072ea82a
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-05 17:32:50 +0200

    Implemented GrozingPliers toolCard

--- src/main/java/it/polimi/se2018/strategy/toolcard/GrozingPliers.java
+++ src/main/java/it/polimi/se2018/strategy/toolcard/GrozingPliers.java
@@ -1,9 +1,36 @@
 package it.polimi.se2018.strategy.toolcard;
 
+import it.polimi.se2018.Dice;
 import it.polimi.se2018.ToolCardEffectStrategy;
 
 public class GrozingPliers implements ToolCardEffectStrategy {
-    public int executeEffect(){
+
+    Dice draftedDice;
+    boolean increase;
+
+    public GrozingPliers(Dice draftedDice, boolean increase) {
+        this.draftedDice = draftedDice;
+        this.increase = increase;
+    }
+
+    public GrozingPliers(GrozingPliers grozingPliers, Dice draftedDice, boolean increase){
+
+        //Non devo riferirmi all'oggetto grozingPliers?????
+
+        this.draftedDice = draftedDice;
+        this.increase = increase;
+    }
+
+
+
+    public int executeEffect() {
+        if (draftedDice.getNumber() != 6 || draftedDice.getNumber() != 1) {
+            if (increase) {
+                draftedDice.setNumber(draftedDice.getNumber() + 1);
+            } else {
+                draftedDice.setNumber(draftedDice.getNumber() - 1);
+            }
+        }
         return 1;
     }
 }
