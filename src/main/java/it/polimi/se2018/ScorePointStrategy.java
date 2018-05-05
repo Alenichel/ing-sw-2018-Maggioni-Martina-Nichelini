eb8aa0e41da8c785af7eb59e458069512759e791
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-05-05 16:01:36 +0200

    Implemented ServerController Singleton

--- src/main/java/it/polimi/se2018/ScorePointStrategy.java
+++ src/main/java/it/polimi/se2018/ScorePointStrategy.java
@@ -1,5 +1,5 @@
 package it.polimi.se2018;
 
 public interface ScorePointStrategy {
-    public int scorePoint();
+    public int scorePoint(WindowPatternCard windowPatternCard);
 }
--- src/main/java/it/polimi/se2018/ServerController.java
+++ src/main/java/it/polimi/se2018/ServerController.java
@@ -1,30 +1,35 @@
 package it.polimi.se2018;
 
 public class ServerController {
-    private final static ServerController instance = new ServerController();
+    private final static ServerController instance = new ServerController(Server.getInstance());
 
     public static ServerController getInstance() {
         return instance;
     }
 
-    private ServerController() {
+    private Server server;
+    private String gameName;
+    private Player admin;
+
+    private ServerController(Server server) {
+        this.server = server;
     }
 
     public void connectPlayer (Player player) {
-        instance.connectPlayer(player);
+        server.addPlayer(player);
     }
 
     public void disconnectPlayer (Player player) {
-        instance.disconnectPlayer(player);
+        server.removePlayer(player);
     }
 
 
     public void createRoom(Room room) {
-        instance.createRoom(room);
+        server.addRoom(String gameName, Player admin);
     }
 
     public void deleteRoom(Room room) {
-        instance.deleteRoom(room);
+        server.removeRoom(room);
     }
 
 }
