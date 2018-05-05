927eb5b62216bd5fbc8f3a2f3c47e18997f87b1f
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-05 18:14:45 +0200

    Added constructor

--- src/main/java/it/polimi/se2018/strategy/toolcard/EnglomiseBrush.java
+++ src/main/java/it/polimi/se2018/strategy/toolcard/EnglomiseBrush.java
@@ -3,6 +3,11 @@ package it.polimi.se2018.strategy.toolcard;
 import it.polimi.se2018.ToolCardEffectStrategy;
 
 public class EnglomiseBrush implements ToolCardEffectStrategy {
+
+    public EnglomiseBrush(){
+
+    }
+
     public int executeEffect(){
         return 0;
     }
