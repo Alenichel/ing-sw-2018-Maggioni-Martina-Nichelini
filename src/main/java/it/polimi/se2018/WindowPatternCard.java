fda54603a65211bfeec0d775f7aeb8d5bc03a95b
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-08 16:11:16 +0200

    Added empty fields in grid

--- src/main/java/it/polimi/se2018/WindowPatternCard.java
+++ src/main/java/it/polimi/se2018/WindowPatternCard.java
@@ -18,26 +18,82 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class WindowPatternCard extends Card{
-    private WindowCell[][] grid = new WindowCell[5][4];
+public class WindowPatternCard extends Card {
+    private WindowCell[][] grid = new WindowCell[4][5];
     private Player player;
-    
+
     private static final String HOME_PATH = System.getProperty("user.home");
     private static final String CONFIGURATION_FILENAME = "/virtus.xml";
 
 
-    public WindowPatternCard (){
+    public WindowPatternCard() {
         try {
             loadConfiguration();
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("[*] Configuration file not found in " + HOME_PATH + CONFIGURATION_FILENAME + "\n[*] Aborting..");
             System.exit(1);
         } catch (IOException | SAXException | ParserConfigurationException e) {
             e.printStackTrace();
         }
+
+        for(WindowCell[] line : grid){
+            for(WindowCell cell : line){
+                if(cell == null){
+                    cell = new WindowCell();
+                }
+            }
+        }
+
     }
 
-    private void loadConfiguration() throws ParserConfigurationException, IOException, SAXException  {
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+
+    public WindowCell getCell(int row, int column) {
+        return grid[row][column];
+    }
+
+    public WindowCell[][] getGrid() {
+        return this.grid;
+    }
+
+    @Override
+    public String toString() {
+        String string ="";
+        string+= "                          \n";
+        string += ""+player.getNickname()+"\n";
+        for (WindowCell[] line : grid) {
+            string+="--------------------------------------------\n";
+            string+="|   ";
+            for (WindowCell cell : line) {
+                if(cell != null) {
+                    if(cell.getAssignedDice() == null) {
+                        //dado non insierito
+                        if (cell.getColorConstraint() != null)
+                            string += cell.getColorConstraint().substring(0, 1);
+                        else if (cell.getNumberConstraint() != 0)
+                            string += cell.getNumberConstraint();
+                        else if (cell.getAssignedDice() != null)
+                            string += " "+cell.getAssignedDice().getNumber()+ "  ";
+                    }else{
+                        //dado inserito
+                        string += cell.getAssignedDice().getNumber() + cell.getAssignedDice().getColor().substring(0,1);
+                    }
+                }
+                else string += "  ";
+                string += "    |   ";
+            }
+            string += "\n";
+        }
+        string+="--------------------------------------------\n";
+        string+= "                          \n";
+        return string;
+    }
+
+
+    private void loadConfiguration() throws ParserConfigurationException, IOException, SAXException {
         File configurationFile = new File(HOME_PATH + CONFIGURATION_FILENAME);
         DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = docBuilderFactory.newDocumentBuilder();
@@ -46,7 +102,7 @@ public class WindowPatternCard extends Card{
         Element virtus = doc.getDocumentElement();
 
         List<String> numberRestrictions = Arrays.asList("one", "two", "three", "four", "five", "six");
-        List<String> colorRestrictions = Arrays.asList("red", "yellow", "green","blue", "purple");
+        List<String> colorRestrictions = Arrays.asList("red", "yellow", "green", "blue", "purple");
 
         NodeList numberList = virtus.getElementsByTagName("number");
         NodeList colorList = virtus.getElementsByTagName("color");
@@ -57,8 +113,8 @@ public class WindowPatternCard extends Card{
         for (String nr : numberRestrictions) {
             NodeList nRestriction = number.getElementsByTagName(nr);
             Element n = (Element) nRestriction.item(0);
-            NodeList coordinateXList = n.getElementsByTagName("x");
-            NodeList coordinateYList = n.getElementsByTagName("y");
+            NodeList coordinateXList = n.getElementsByTagName("y");
+            NodeList coordinateYList = n.getElementsByTagName("x");
             Element coordinateX = (Element) coordinateXList.item(0);
             Element coordinateY = (Element) coordinateYList.item(0);
 
@@ -67,7 +123,7 @@ public class WindowPatternCard extends Card{
             List<String> axs;
             List<String> ays;
 
-            if (coordinateX != null ) {
+            if (coordinateX != null) {
                 xs = coordinateX.getFirstChild().getNodeValue();
                 ys = coordinateY.getFirstChild().getNodeValue();
 
@@ -82,11 +138,11 @@ public class WindowPatternCard extends Card{
             }
         }
 
-        for (String cr : colorRestrictions){
+        for (String cr : colorRestrictions) {
             NodeList cRestriction = color.getElementsByTagName(cr);
             Element c = (Element) cRestriction.item(0);
-            NodeList coordinateXList = c.getElementsByTagName("x");
-            NodeList coordinateYList = c.getElementsByTagName("y");
+            NodeList coordinateXList = c.getElementsByTagName("y");
+            NodeList coordinateYList = c.getElementsByTagName("x");
             Element coordinateX = (Element) coordinateXList.item(0);
             Element coordinateY = (Element) coordinateYList.item(0);
 
@@ -100,7 +156,7 @@ public class WindowPatternCard extends Card{
                 axs = Arrays.asList(xs.split(" "));
                 ays = Arrays.asList(ys.split(" "));
 
-                for (int i = 0; i<axs.size(); i++){
+                for (int i = 0; i < axs.size(); i++) {
                     if (axs != null) {
                         grid[Integer.parseInt(axs.get(i)) - 1][Integer.parseInt(ays.get(i)) - 1] = new WindowCell(cr);
                     } //end if
@@ -109,12 +165,5 @@ public class WindowPatternCard extends Card{
         }//end method
     }
 
-    public WindowCell getCell(int row, int column){
-        return null;
-    }
-
-    public WindowCell[][] getGrid(){
-        return this.grid;
-    }
 
 }
