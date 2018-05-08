c56e2966c5b0a4f2c7ce8c94eff3202c052d46e9
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-08 12:37:06 +0200

    Added configuration load from XML

--- src/main/java/it/polimi/se2018/WindowPatternCard.java
+++ src/main/java/it/polimi/se2018/WindowPatternCard.java
@@ -2,17 +2,24 @@ package it.polimi.se2018;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class WindowPatternCard extends Card{
-    private ArrayList<ArrayList<WindowCell>> composition;
+    private WindowCell[][] grid = new WindowCell[5][4];
     private Player player;
     
     private static final String HOME_PATH = System.getProperty("user.home");
@@ -31,23 +38,82 @@ public class WindowPatternCard extends Card{
     }
 
     private void loadConfiguration() throws ParserConfigurationException, IOException, SAXException  {
-
         File configurationFile = new File(HOME_PATH + CONFIGURATION_FILENAME);
-        Document doc1 = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(configurationFile);
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = documentBuilder.parse(configurationFile);
 
-        //element.add(Integer.parseInt(doc1.getElementsByTagName("three").item(0).getTextContent()));
-        //element.add(Integer.parseInt(doc1.getElementsByTagName("y").item(0).getTextContent()));
-        //doc1.getElementsByTagName("uno");
+        Element virtus = doc.getDocumentElement();
 
-        //System.out.println(doc1.getElementsByTagName("four").item(0).getAttributes().getNamedItem("pos"));
-        //System.out.print(doc1.getElementsByTagName("one").item(0).getTextContent().substring(9,12));
-        int x = Integer.parseInt(doc1.getElementsByTagName("one").item(0).getTextContent().substring(9,10));
-        int y = Integer.parseInt(doc1.getElementsByTagName("one").item(0).getTextContent().substring(11,12));
+        List<String> numberRestrictions = Arrays.asList("one", "two", "three", "four", "five", "six");
+        List<String> colorRestrictions = Arrays.asList("red", "yellow", "green","blue", "purple");
 
-        System.out.println("x: " + x +" y: " + y);
+        NodeList numberList = virtus.getElementsByTagName("number");
+        NodeList colorList = virtus.getElementsByTagName("color");
+        Element number = (Element) numberList.item(0);
+        Element color = (Element) colorList.item(0);
+
+        int counter = 1;
+        for (String nr : numberRestrictions) {
+            System.out.println(nr);
+            NodeList nRestriction = number.getElementsByTagName(nr);
+            Element n = (Element) nRestriction.item(0);
+            NodeList coordinateXList = n.getElementsByTagName("x");
+            NodeList coordinateYList = n.getElementsByTagName("y");
+            Element coordinateX = (Element) coordinateXList.item(0);
+            Element coordinateY = (Element) coordinateYList.item(0);
+
+            String xs;
+            String ys;
+            List<String> axs;
+            List<String> ays;
+
+            if (coordinateX != null ) {
+                xs = coordinateX.getFirstChild().getNodeValue();
+                ys = coordinateY.getFirstChild().getNodeValue();
+
+                axs = Arrays.asList(xs.split(" "));
+                ays = Arrays.asList(ys.split(" "));
 
 
-        System.out.print(doc1.getElementsByTagName("two").item(0).getTextContent());
+                for (int i = 0; i < axs.size(); i++) {
+                    grid[Integer.parseInt(axs.get(i)) - 1][Integer.parseInt(ays.get(i)) - 1] = new WindowCell(counter);
+                }
+                counter++;
+                System.out.println(axs);
+                System.out.println(ays);
+            }
+        }
+
+        for (String cr : colorRestrictions){
+            System.out.println(cr);
+            NodeList cRestriction = color.getElementsByTagName(cr);
+            Element c = (Element) cRestriction.item(0);
+            NodeList coordinateXList = c.getElementsByTagName("x");
+            NodeList coordinateYList = c.getElementsByTagName("y");
+            Element coordinateX = (Element) coordinateXList.item(0);
+            Element coordinateY = (Element) coordinateYList.item(0);
+
+            String xs;
+            String ys;
+            List<String> axs;
+            List<String> ays;
+            if (coordinateX != null) { //check if there are null values;
+                xs = coordinateX.getFirstChild().getNodeValue();
+                ys = coordinateY.getFirstChild().getNodeValue();
+                axs = Arrays.asList(xs.split(" "));
+                ays = Arrays.asList(ys.split(" "));
+
+                for (int i = 0; i<axs.size(); i++){
+                    if (axs != null) {
+                        grid[Integer.parseInt(axs.get(i)) - 1][Integer.parseInt(ays.get(i)) - 1] = new WindowCell(cr);
+                    } //end if
+                }//end for
+
+                System.out.println(axs);
+                System.out.println(ays);
+            }//end if
+        }//end method
     }
 
     public WindowCell getCell(int row, int column){
