3d408e1503e58ae505c85fa76188b7f2cd23245a
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-09 16:26:45 +0200

    Created forbiddenDiceInsert exception

---
+++ src/main/java/it/polimi/se2018/Exception/ForbiddenDiceInsert.java
@@ -0,0 +1,9 @@
+package it.polimi.se2018.Exception;
+
+import it.polimi.se2018.WindowCell;
+
+public class ForbiddenDiceInsert extends ToolCardException{
+    public ForbiddenDiceInsert(String message){
+        super(message);
+    }
+}
