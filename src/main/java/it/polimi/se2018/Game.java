053738fcda8d760aeb09f9555fe1a63d9cc73306
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-09 15:54:27 +0200

    Game prototype ready

--- src/main/java/it/polimi/se2018/Game.java
+++ src/main/java/it/polimi/se2018/Game.java
@@ -1,53 +1,49 @@
 package it.polimi.se2018;
-import java.util.ArrayList;
-import java.util.Random;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
-
-    private int id;
     private ArrayList<Dice> diceBag = new ArrayList<>();
     private ArrayList<Dice> diceOnTable = new ArrayList<>();
     private ArrayList<WindowPatternCard> patternCards = new ArrayList<>();
     private ArrayList<ObjectiveCard> objectiveCards = new ArrayList<>();
     private ArrayList<ToolCard> toolCards = new ArrayList<>();
+    private final List<Player> players = new ArrayList<>();
 
-    private ArrayList<Player> roundOrderPlayers = new ArrayList<>(); //turn handling
-    private Player activePlayer; // turn handling
 
-    public Game() {
-        this.id = this.hashCode();
+    //private Player activePlayer;
 
+    public Game(List<Player> players){
+        this.players = players;
     }
 
-    public void addPlayerToGame(Player player){
-        roundOrderPlayers.add(player);
+    public ArrayList<Dice> getDiceBag() {
+        return diceBag;
     }
 
-    public void removePlayerFromGame(Player player){
-        roundOrderPlayers.remove(player);
+    public ArrayList<Dice> getDiceOnTable() {
+        return diceOnTable;
     }
 
-    public Player getActivePlayer(){
-        return activePlayer;
+    public ArrayList<WindowPatternCard> getPatternCards() {
+        return patternCards;
     }
 
-    public void setActivePlayer(Player player){
-        this.activePlayer = player;
+    public void addWindowPatternCard(WindowPatternCard windowPatternCard){
+        this.patternCards.add(windowPatternCard);
     }
 
-    public void setRoundOrderPlayers(){
-        //to be implemented
+    public ArrayList<ObjectiveCard> getObjectiveCards() {
+        return objectiveCards;
     }
 
-    public void draftDice(int nOfDice){
-        Random randomizer = new Random();
-        for (int i = 0; i<nOfDice; i++) {
-            Dice dice = diceBag.get(randomizer.nextInt(diceBag.size()));
-            diceBag.remove(dice);
-            diceOnTable.add(dice);
-        }
+    public ArrayList<ToolCard> getToolCards() {
+        return toolCards;
     }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
 }
-
-
