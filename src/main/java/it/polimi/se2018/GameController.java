5c6d2d43155aa7ffe06d6ebaa8d46beafe3b3df8
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-09 15:44:23 +0200

    Added method onSelectionMessage

--- src/main/java/it/polimi/se2018/GameController.java
+++ src/main/java/it/polimi/se2018/GameController.java
@@ -1,5 +1,9 @@
 package it.polimi.se2018;
 
+import it.polimi.se2018.message.Message;
+import it.polimi.se2018.message.SelectionMessage;
+
+import java.io.InvalidClassException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observer;
@@ -8,14 +12,18 @@ import java.util.Observable;
 public class GameController implements Observer{
     private Game gameReference;
 
+    //manca il metodo che serve le pattern card.
+
     public GameController(Game gameReference){
         ArrayList<WindowPatternCard> initializedPatternCards = new ArrayList<>();
         this.gameReference = gameReference;
         initializedPatternCards = initializedPatternCard();
     }
 
-    public ArrayList<WindowPatternCard> initializedPatternCard(){
+    private ArrayList<WindowPatternCard> initializedPatternCard(){
         ArrayList<String> windowsPatternCardsName = new ArrayList<>();
+        ArrayList<WindowPatternCard> initializedPatternCards = new ArrayList<>();
+
         windowsPatternCardsName.add("virtus");
         windowsPatternCardsName.add("viaLux");
         windowsPatternCardsName.add("bellesguard");
@@ -29,37 +37,36 @@ public class GameController implements Observer{
         windowsPatternCardsName.add("industria");
         windowsPatternCardsName.add("symhonyOfLight");
 
-        ArrayList<WindowPatternCard> initializedPatternCards = new ArrayList<>();
-
         for (String str : windowsPatternCardsName){
             initializedPatternCards.add(new WindowPatternCard(str));
         }
-    return initializedPatternCards;
+        return initializedPatternCards;
     }
 
-
-
-    public void gameSetupHandler(){
-        /*
-      -- istanzia tutte le carte obiettivi (pubbliche e private)
-      -- istanzia la room con i giocatori
-      -- istanzia tutti i dati nella bag
-      -- istanzia 3 carte tool
-      -- istanzia il turn robo
-
-      */
-    }
-
-    public void gameHandler(){
-
-    }
-
-    public void endingHandler(){
-
+    private void onPatternCardSelection(SelectionMessage message) throws InvalidClassException {
+        if (message.getChosenItem() instanceof WindowPatternCard ){
+            Player targetPlayer = gameReference.getPlayers().get(message.getPlayerNumber());
+            targetPlayer.setActivePatternCard((WindowPatternCard)(message.getChosenItem()));
+            targetPlayer.setPatternCardPool(null);
+            this.gameReference.notifyAll();
+        } else {
+            throw new InvalidClassException("Received: " + message.getChosenItem() + "but requested WindowPatternCard");
+        }
     }
 
    @Override
     public void update(Observable observable, Object msg){
 
+        switch(((Message)msg).getMessageType()){
+
+            case "SelectionMessage":
+                try {
+                    this.onPatternCardSelection((SelectionMessage) msg);
+                } catch (InvalidClassException e){}
+                break;
+
+            default:
+                throw new RuntimeException();
+        }
     }
 }
