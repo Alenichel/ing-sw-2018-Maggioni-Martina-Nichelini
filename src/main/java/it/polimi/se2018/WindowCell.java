e0b5137a830c277f75909ba1c69c58e3d5c3c7f9
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-10 13:31:46 +0200

    Added neighbours saving

--- src/main/java/it/polimi/se2018/WindowCell.java
+++ src/main/java/it/polimi/se2018/WindowCell.java
@@ -1,26 +1,39 @@
 package it.polimi.se2018;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class WindowCell {
-    //----servono?----
     private int row;
     private int column;
-    //----------------
     private String colorConstraint;
     private int numberConstraint;
     private Dice assignedDice;
 
+    private ArrayList<WindowCell> neighbourCells = new ArrayList<>();
 
     public WindowCell(){
     }
 
-    public WindowCell(int numberConstraint) {
+    private void setNeighbours(WindowCell[][] grid, int x, int y){
+        if (x - 1 >= 0 ) neighbourCells.add(grid[x-1][y]);
+        if (x + 1 < 4 ) neighbourCells.add(grid[x+1][y]);
+        if (y - 1 >= 0 ) neighbourCells.add(grid[x][y-1]);
+        if (y + 1 < 5 ) neighbourCells.add(grid[x][y+1]);
+    }
+
+    public WindowCell(WindowCell[][] grid, int row, int column, int numberConstraint) {
+        setNeighbours(grid, row, column);
+        this.row = row;
+        this.column = column;
         this.numberConstraint = numberConstraint;
         this.colorConstraint = null;
     }
 
-    public WindowCell(String colorConstraint){
+    public WindowCell(WindowCell[][] grid, int row, int column, String colorConstraint){
+        setNeighbours(grid, row, column);
+        this.row = row;
+        this.column = column;
         this.numberConstraint = 0;
         this.colorConstraint = colorConstraint;
     }
@@ -43,6 +56,10 @@ public class WindowCell {
 
     public Dice getAssignedDice() {
         return assignedDice;
+    }
+
+    public ArrayList<WindowCell> getNeighbourCells() {
+        return neighbourCells;
     }
 
     public void setAssignedDice(Dice assignedDice) {
