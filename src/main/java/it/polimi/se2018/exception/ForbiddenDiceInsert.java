1bed01e2ecad9fb0fca29422eaa8d42436648247
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-11 11:44:07 +0200

    NameChanged

--- src/main/java/it/polimi/se2018/exception/ForbiddenDiceInsert.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.se2018.exception;
-
-public class ForbiddenDiceInsert extends ToolCardException{
-    public ForbiddenDiceInsert(String message){
-        super(message);
-    }
-}
---
+++ src/main/java/it/polimi/se2018/exception/NotValidInsertion.java
@@ -0,0 +1,7 @@
+package it.polimi.se2018.exception;
+
+public class NotValidInsertion extends ToolCardException{
+    public NotValidInsertion(String message){
+        super(message);
+    }
+}
