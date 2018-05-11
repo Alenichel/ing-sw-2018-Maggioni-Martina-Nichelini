75ab7ab5e0b323452ef092135b37fb31053f5e55
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-11 18:10:27 +0200

    Changed package exception

--- src/main/java/it/polimi/se2018/ToolCardEffectStrategy.java
+++ src/main/java/it/polimi/se2018/ToolCardEffectStrategy.java
@@ -1,6 +1,6 @@
 package it.polimi.se2018;
 
-import it.polimi.se2018.Exception.ToolCardException;
+import it.polimi.se2018.exception.ToolCardException;
 
 public interface ToolCardEffectStrategy {
     public int executeEffect() throws ToolCardException;
