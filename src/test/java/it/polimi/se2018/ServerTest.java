d64d561c36b142429325ca0549d83213c6866840
valentinamaggioni <valentina3.maggioni@mail.polimi.it>
2018-05-13 11:25:40 +0200

    Added ServerTest class

---
+++ src/test/java/it/polimi/se2018/ServerTest.java
@@ -0,0 +1,42 @@
+package it.polimi.se2018;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class ServerTest {
+
+    @Test
+    public void testGetter() {
+        Server s = new Server();
+        String gameName = "game";
+        Player p = new Player("admin");
+        Room r = new Room("room", p, false);
+
+        Assert.assertEquals( 5050, s.getServerPort());
+        Assert.assertNotEquals(2018, s.getServerPort());
+
+        Assert.assertEquals( 120, s.getDefaultMatchmakingTimer());
+        Assert.assertNotEquals(100, s.getDefaultMatchmakingTimer());
+
+        Assert.assertEquals(60, s.getDefaultMoveTimer());
+        Assert.assertNotEquals(90, s.getDefaultMoveTimer());
+
+        s.addPlayer(p);
+        Assert.assertTrue(s.getOnlinePlayers().contains(p));
+
+        s.removePlayer(p);
+        Assert.assertTrue(!s.getOnlinePlayers().contains(p));
+
+        s.addRoom(gameName, p);
+        Assert.assertEquals(true, s.getActiveGames().contains(r));
+
+        /*removeRoom(gameName, p);
+        Assert.assertEquals(true, !s.getActiveGames().contains(r));*/
+
+    }
+
+}
