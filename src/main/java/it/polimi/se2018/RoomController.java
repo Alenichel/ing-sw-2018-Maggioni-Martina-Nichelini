7275a93b3a4d117f5012a07fcdb969b9439a97b7
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-13 17:58:04 +0200

    Added JavaDoc

--- src/main/java/it/polimi/se2018/RoomController.java
+++ src/main/java/it/polimi/se2018/RoomController.java
@@ -5,36 +5,69 @@ import it.polimi.se2018.message.Message;
 import java.util.Observer;
 import java.util.Observable;
 
+/**
+ * Controller for class Room implemented trough a StatePattern.
+ * Room controller is an observer of RoomView.
+ */
 public class RoomController implements Observer {
 
     public final Room room;
     private RoomControllerState state;
 
+    /**
+     *
+     * @param state The initial state tou want for ypur class
+     * @param room  The Room model class you the controller to be associated
+     */
     public RoomController(RoomControllerState state, Room room){
         this.room = room;
         this.state = state;
     }
 
+    /**
+     * Launch the game depending on the current room controller state.
+     */
     private void launchGame(){
         state.launchGame(this);
     }
 
+    /**
+     * Connect a player depending on the current room controller state.
+     * @param player
+     */
     private void connectPlayer( Player player){
         state.connectPlayer(this, player);
     }
 
+    /**
+     * Disconecct a player depending on the current room controller state.
+     * @param player
+     */
     private void disconnectPlayer(Player player){
         state.disconnectPlayer(this, player);
     }
 
+    /**
+     * Change the state of the room controller.
+     * @param state The new state
+     */
     public void setState(RoomControllerState state){
         this.state = state;
     }
 
+    /**
+     * State getter.
+     * @return the current room controller state.
+     */
     public RoomControllerState getState(){
         return this.state;
     }
 
+    /**
+     * Umpdate method to handle message coming from the view
+     * @param observable
+     * @param msg
+     */
     public void update(Observable observable, Object msg){
         switch(((Message)msg).getMessageType()){
             case "ConnectionMessage":
