119bfb7b59429c5bdf0e2633f4ed116adc4641ad
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-13 18:05:02 +0200

    Added JavaDoc

--- src/main/java/it/polimi/se2018/RoomWaitingState.java
+++ src/main/java/it/polimi/se2018/RoomWaitingState.java
@@ -1,7 +1,14 @@
 package it.polimi.se2018;
 
+/**
+ * Concrete State for RooController's State Pattern.
+ */
 public class RoomWaitingState implements RoomControllerState{
 
+    /**
+     * This method handles the launch of a new game
+     * @param context
+     */
     @Override
     public void launchGame(RoomController context) {
         Game newGame = new Game(context.room.getListOfConnectedPlayers());
@@ -10,11 +17,21 @@ public class RoomWaitingState implements RoomControllerState{
         context.setState(new RoomWaitingState());
     }
 
+    /**
+     * Connection handling when the room is waiting.
+     * @param context
+     * @param player
+     */
     @Override
     public void connectPlayer(RoomController context, Player player) {
         context.room.addPlayer(player);
     }
 
+    /**
+     * Disconnection handling when the room is waiting.
+     * @param context
+     * @param player
+     */
     @Override
     public void disconnectPlayer(RoomController context, Player player) {
         context.room.removePlayer(player);
