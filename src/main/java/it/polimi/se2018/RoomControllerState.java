524c873497ab21057e3df4cad088ac875177781d
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-13 17:58:48 +0200

    Added JavaDoc

--- src/main/java/it/polimi/se2018/RoomControllerState.java
+++ src/main/java/it/polimi/se2018/RoomControllerState.java
@@ -1,5 +1,8 @@
 package it.polimi.se2018;
 
+/**
+ * Interface for the implementation of RoomController's state pattern
+ */
 public interface RoomControllerState {
     void launchGame(RoomController context);
     void connectPlayer(RoomController context, Player player);
