fdcc97c8d32844c7b87d8b51f8c7f338238c09e8
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-13 18:48:07 +0200

    Updated class

--- src/main/java/it/polimi/se2018/message/RoomConnectionMessage.java
+++ src/main/java/it/polimi/se2018/message/RoomConnectionMessage.java
@@ -6,13 +6,19 @@ import it.polimi.se2018.Room;
 public class RoomConnectionMessage extends Message{
 
     private Player requester;
+    private boolean isConnecting;
 
-    public RoomConnectionMessage(Player requester){
-        this.messageType = "ConnectionRequest";
+    public RoomConnectionMessage(Player requester, boolean isConnecting){
+        this.messageType = "ConnectionMessage";
         this.requester = requester;
+        this.isConnecting = isConnecting;
     }
 
     public Player getRequester() {
         return requester;
+    }
+
+    public boolean isConnecting() {
+        return isConnecting;
     }
 }
