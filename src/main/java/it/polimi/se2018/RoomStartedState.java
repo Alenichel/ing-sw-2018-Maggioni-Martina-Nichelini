7fafff3af297af4f4d95654d125766fe4978164a
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-13 18:02:25 +0200

    Added JavaDoc

--- src/main/java/it/polimi/se2018/RoomStartedState.java
+++ src/main/java/it/polimi/se2018/RoomStartedState.java
@@ -1,12 +1,24 @@
 package it.polimi.se2018;
 
+/**
+ * Concrete State for RooController's State Pattern.
+ */
 public class RoomStartedState implements RoomControllerState{
 
+    /**
+     * This method  has to fail when the game is already started.
+     * @param context
+     */
     @Override
     public void launchGame(RoomController context) {
         throw new RuntimeException("Game already started");
     }
 
+    /**
+     * This method let only previously disconnected client to be reconnected to the game.
+     * @param context
+     * @param player The player to be added
+     */
     @Override
     public void connectPlayer(RoomController context, Player player) {
         if (context.room.isADisconnectedClient(player)){
@@ -17,6 +29,11 @@ public class RoomStartedState implements RoomControllerState{
         }
     }
 
+    /**
+     * This method handles the disconnection process when the game associated with this room is started
+     * @param context
+     * @param player
+     */
     @Override
     public void disconnectPlayer(RoomController context, Player player) {
         context.room.addDisconnectedClient(player);
