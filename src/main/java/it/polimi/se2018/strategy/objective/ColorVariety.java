65cc992f829169cadc7a8526ad92079a77427998
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-13 17:01:10 +0200

    Reduced complexity

--- src/main/java/it/polimi/se2018/strategy/objective/ColorVariety.java
+++ src/main/java/it/polimi/se2018/strategy/objective/ColorVariety.java
@@ -12,8 +12,7 @@ public class ColorVariety implements ScorePointStrategy {
         if(a==null) return false;
         if(a.getAssignedDice()==null) return false;
         else {
-            if (a.getAssignedDice().getColor()== "red") return true;
-            else return false;
+            return (a.getAssignedDice().getColor()== "red");
         }
     }
 
@@ -21,8 +20,7 @@ public class ColorVariety implements ScorePointStrategy {
         if(a==null) return false;
         if(a.getAssignedDice()==null) return false;
         else {
-            if (a.getAssignedDice().getColor()== "yellow") return true;
-            else return false;
+            return (a.getAssignedDice().getColor()== "yellow");
         }
     }
 
@@ -30,8 +28,7 @@ public class ColorVariety implements ScorePointStrategy {
         if(a==null) return false;
         if(a.getAssignedDice()==null) return false;
         else {
-            if (a.getAssignedDice().getColor()== "blue") return true;
-            else return false;
+            return (a.getAssignedDice().getColor()== "blue");
         }
     }
 
@@ -39,8 +36,7 @@ public class ColorVariety implements ScorePointStrategy {
         if(a==null) return false;
         if(a.getAssignedDice()==null) return false;
         else {
-            if (a.getAssignedDice().getColor()== "green") return true;
-            else return false;
+            return (a.getAssignedDice().getColor()== "green");
         }
     }
 
@@ -48,8 +44,7 @@ public class ColorVariety implements ScorePointStrategy {
         if(a==null) return false;
         if(a.getAssignedDice()==null) return false;
         else {
-            if (a.getAssignedDice().getColor()== "purple") return true;
-            else return false;
+            return (a.getAssignedDice().getColor()== "purple");
         }
     }
 
@@ -73,34 +68,23 @@ public class ColorVariety implements ScorePointStrategy {
         WindowCell[][] grid = windowPatternCard.getGrid();
 
         for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < 5; j++) {
                 if (isRed(grid[i][j])) {
                     red += 1;
                 }
-
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++)
                 if (isYellow(grid[i][j])) {
                     yellow +=1;
                 }
-
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++)
                 if (isBlue(grid[i][j])) {
                     blue +=1;
                 }
-
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++)
                 if (isGreen(grid[i][j])) {
                     green +=1;
                 }
-
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++)
                 if (isPurple(grid[i][j])) {
                     purple +=1;
                 }
+            }
 
         scoreCounter = 4*fiveMin(red, yellow, blue, green, purple);
         return scoreCounter;
