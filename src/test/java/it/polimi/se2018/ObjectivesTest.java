49b078a82b229cc508b238eefbb65cab9a35d78a
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-14 22:06:29 +0200

    Added test for shade variety

--- src/test/java/it/polimi/se2018/ObjectivesTest.java
+++ src/test/java/it/polimi/se2018/ObjectivesTest.java
@@ -1,10 +1,12 @@
 package it.polimi.se2018;
 
+import com.sun.tools.corba.se.idl.constExpr.Not;
 import it.polimi.se2018.exception.NotEmptyWindowCellException;
 import it.polimi.se2018.exception.NotValidInsertion;
 import it.polimi.se2018.strategy.objective.ColorVariety;
 import it.polimi.se2018.strategy.objective.ColumnVariety;
 import it.polimi.se2018.strategy.objective.RowVariety;
+import it.polimi.se2018.strategy.objective.ShadeVariety;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -93,6 +95,30 @@ public class ObjectivesTest {
         ScorePointStrategy sps = new ColorVariety();
         Assert.assertEquals(4,sps.scorePoint(wpc));
 
+    }
+
+    @Test
+    public void testShadeVariety() throws NotValidInsertion, NotEmptyWindowCellException{
+        WindowPatternCard wpc = new WindowPatternCard("auroraeMagnificus");
+
+        Dice d1 = new Dice("purple", 1);
+        Dice d2 = new Dice("yellow",2);
+        Dice d3 = new Dice("red",3);
+        Dice d4 = new Dice("blue",4);
+        Dice d5 = new Dice("green",5);
+        Dice d6 = new Dice("green", 6);
+
+        wpc.insertDice(d1, 0, 0, false,false);
+        wpc.insertDice(d2, 1,1,false,false);
+        wpc.insertDice(d3, 2,2,false,false);
+        wpc.insertDice(d4, 3,3,false,false);
+        wpc.insertDice(d5, 3,2,false,false);
+        wpc.insertDice(d6, 0,1,false,false);
+
+        wpc.insertDice(d1, 3,4, false,false);
+        wpc.insertDice(d1, 2,1,false,false);
+        ScorePointStrategy sps = new ShadeVariety();
+        Assert.assertEquals(5,sps.scorePoint(wpc));
     }
 
     @Test
