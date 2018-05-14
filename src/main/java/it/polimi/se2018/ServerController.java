22a60e841d4f904843cdbedf6cce3b4c1e243e81
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-14 14:10:15 +0200

    ServerController now compliant with observer pattern

--- src/main/java/it/polimi/se2018/ServerController.java
+++ src/main/java/it/polimi/se2018/ServerController.java
@@ -1,37 +1,52 @@
 package it.polimi.se2018;
 
-public class ServerController {
+import it.polimi.se2018.message.ConnectionMessage;
+import it.polimi.se2018.message.Message;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ServerController implements Observer{
     private final static ServerController instance = new ServerController(Server.getInstance());
+    private Server server;
 
     public static ServerController getInstance() {
         return instance;
     }
 
-    private Server server;
-    private String gameName;
-    private Player admin;
-
     private ServerController(Server server) {
         this.server = server;
     }
 
-    public void connectPlayer (Player player) {
+    private void connectPlayer (Player player) {
         server.addPlayer(player);
         player.setOnline(true);
     }
 
-    public void disconnectPlayer (Player player) {
+    private void disconnectPlayer (Player player) {
         server.removePlayer(player);
         player.setOnline(false);
     }
 
 
-    public void createRoom(String gameName, Player admin) {
+    private void createRoom(String gameName, Player admin) {
         server.addRoom(gameName, admin);
     }
 
-    public void deleteRoom(Room room) {
+    private void deleteRoom(Room room) {
         server.removeRoom(room);
+    }
+
+
+    public void update (Observable o, Object msg){
+        switch(((Message)msg).getMessageType()){
+            case "ConnectionMessage":
+                this.disconnectPlayer( ((ConnectionMessage)msg).getRequester() );
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
