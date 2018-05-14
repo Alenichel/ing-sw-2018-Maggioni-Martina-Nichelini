d489e126cc4130a6218c441bdb4b9d5568e07d60
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-14 13:49:43 +0200

    Added new message class

---
+++ src/main/java/it/polimi/se2018/message/GiveMessage.java
@@ -0,0 +1,21 @@
+package it.polimi.se2018.message;
+
+public class GiveMessage extends Message {
+
+    private Object givenObject;
+    private String giving;
+
+    public GiveMessage(String giving, Object givenObject){
+        this.givenObject = givenObject;
+        this.messageType = "GiveMessage";
+        this.giving = giving;
+    }
+
+    public Object getGivenObject() {
+        return givenObject;
+    }
+
+    public String getGiving() {
+        return giving;
+    }
+}
