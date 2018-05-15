b350e2b9e8bc2518c75c542880a7ce3ef7720feb
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-15 17:33:08 +0200

    Added connecting function

--- src/main/java/it/polimi/se2018/CliView.java
+++ src/main/java/it/polimi/se2018/CliView.java
@@ -1,18 +1,20 @@
 package it.polimi.se2018;
 
-import it.polimi.se2018.message.GetMessage;
+import it.polimi.se2018.message.RequestMessage;
 import it.polimi.se2018.message.GiveMessage;
 import it.polimi.se2018.message.Message;
 import it.polimi.se2018.message.ConnectionMessage;
 
 import java.util.*;
 
-public class CliView extends Observable implements Observer {
+public class CliView extends View implements Observer {
 
     Player client;
     private Observer SCObserver;
     private Observer RCobserver;
     private Observer GCObserver;
+
+    Object lastObjectReceveid;
 
     public CliView(Player client){
         this.client = client;
@@ -25,14 +27,34 @@ public class CliView extends Observable implements Observer {
         while (true){
             String input = sinput.nextLine();
 
+
             if (input.equalsIgnoreCase("RequestConnection")){
                 if (RCobserver != null)
                     RCobserver.update(this, new ConnectionMessage(this.client, true));
             }
+            else if (input.equalsIgnoreCase("Connect")){
+                while (true) {
+                    input = sinput.nextLine();
+                    if (input.equalsIgnoreCase("abort")) break;
+                    int index = 0;
+                    try {
+                        index = Integer.parseInt(input);
+                        ConnectionMessage msg = new ConnectionMessage(client, ((ArrayList<Room>) this.lastObjectReceveid).get(index-1), true);
+                        SCObserver.update(this, msg);
+                    } catch (NumberFormatException |  IndexOutOfBoundsException e){
+                        System.out.println("Not Valid Index");
+                        continue;
+                    }
+                    break;
+                }
+            }
             else if (input.equalsIgnoreCase("GetConnectedPlayers")){
                 if (RCobserver != null)
-                    RCobserver.update(this, new GetMessage("ConnectedPlayers"));
+                    RCobserver.update(this, new RequestMessage("ConnectedPlayers"));
                 else System.out.println("[*] ERROR: you are not connected to a room");
+            }
+            else if (input.equalsIgnoreCase("GetActiveRooms")){
+                SCObserver.update(this, new RequestMessage("ActiveRooms"));
             }
             else if (input.equalsIgnoreCase("Quit")){
                 if (RCobserver == null && GCObserver == null)
@@ -51,16 +73,16 @@ public class CliView extends Observable implements Observer {
         this.RCobserver = o;
     }
 
-    public void RCCallback(Message callbackMessage){
-        if (callbackMessage instanceof GiveMessage){
-            Object o = ((GiveMessage)callbackMessage).getGivenObject();
-
-            if (((GiveMessage)callbackMessage).getGiving().equals("Players")){
-                System.out.println(o.toString());
-            }
-
-        }
+    public void requestCallback(GiveMessage callbackMessage){
+        this.lastObjectReceveid = callbackMessage.getGivenObject();
+        System.out.println(this.lastObjectReceveid);
     }
+
+
+
+
+
+
 
     public void update(Observable o, Object msg){
         System.out.println(((Message)msg).toString());
