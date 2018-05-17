d054515a77298fc4a59626b99c5efedf8f42c22d
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-17 16:02:44 +0200

    Added abastract method

--- src/main/java/it/polimi/se2018/view/View.java
+++ src/main/java/it/polimi/se2018/view/View.java
@@ -1,15 +1,21 @@
 package it.polimi.se2018.view;
 
-import it.polimi.se2018.message.GiveMessage;
+import it.polimi.se2018.message.Message;
 import it.polimi.se2018.model.Player;
 
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Observable;
+import java.util.Observer;
 
-public abstract class View extends Observable {
+public abstract class View extends Observable implements Serializable {
 
     protected Player client;
 
-    public abstract void requestCallback(GiveMessage giveMessage);
+    public abstract void controllerCallback(Message msg);
+
+    public abstract void update(Observable observable, Object message);
 
     public Player getClient() {
         return client;
