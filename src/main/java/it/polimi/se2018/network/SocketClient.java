081b7d2586bd7c95abc67a0080792fd1d29a6913
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-18 10:45:35 +0200

    Corrected error of last commit

---
+++ src/main/java/it/polimi/se2018/network/SocketClient.java
@@ -0,0 +1,78 @@
+package it.polimi.se2018.network;
+
+import it.polimi.se2018.message.*;
+import it.polimi.se2018.view.View;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+public class SocketClient extends Thread implements Observer {
+
+    private String serverIP;
+    private int port;
+    private Socket socket;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private boolean clientConnected;
+    Scanner stdin = new Scanner(System.in);
+
+    private View associatedView;
+
+    public SocketClient(String serverIP, int port, View associateView) {
+        try {
+            this.serverIP = serverIP;
+            this.port = port;
+            this.associatedView = associateView;
+
+            socket = new Socket(serverIP, port);
+            System.out.println("[*] Socket ready..");
+            System.out.println("[*] Connection established");
+            this.oos = new ObjectOutputStream(socket.getOutputStream());
+            this.ois = new ObjectInputStream(socket.getInputStream());
+            oos.writeObject(new HandshakeConnectionMessage("Alenichellll", "password"));
+        } catch (IOException e) {
+            System.out.println(e + "/n" + "[*] Error, exiting..");
+        }
+        Listener listener = new Listener();
+        listener.start();
+
+    }
+
+
+    private class Listener extends Thread{
+
+        public void run(){
+            Object in = null;
+            while (!socket.isClosed()){
+                try {
+                    in = ois.readObject();
+                } catch (ClassNotFoundException | IOException e){System.out.println(e);}
+
+                if (in instanceof SocketUpdateContainer){
+                    SocketUpdateContainer suc = (SocketUpdateContainer)in;
+                    associatedView.update(suc.getObservable(), suc.getObject());
+                }
+                else if (in instanceof Message){
+                    associatedView.controllerCallback((Message)in);
+                }
+            }
+        }
+    }
+
+    public void update(Observable o, Object msg) {
+        SocketUpdateContainer suc = new SocketUpdateContainer(o, msg);
+        try {
+            this.oos.writeObject(suc);
+            this.oos.flush();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
