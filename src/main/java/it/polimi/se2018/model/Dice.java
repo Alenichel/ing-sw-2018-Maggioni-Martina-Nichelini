6346af62c4b0a8d2d2031fbc43f37e80e71aeae2
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-18 15:33:58 +0200

    Added toString method

--- src/main/java/it/polimi/se2018/model/Dice.java
+++ src/main/java/it/polimi/se2018/model/Dice.java
@@ -8,11 +8,13 @@ public class Dice {
     private int number;
     private String color;
     private String location;
+    private String unicode = "";
 
     public Dice(String color){
         this.color = color;
         this.location = null;
         this.rollDice();
+        this.toUnicode(this.color, this.getNumber());
     }
 
     public Dice(String color, int number){
@@ -44,20 +46,42 @@ public class Dice {
     public void rollDice(){
         Random rand = new Random();
         this.number =rand.nextInt(4)+1;
+        this.toUnicode(this.color, this.number);
     }
 
+    private void toUnicode(String color, int n){
+        this.unicode = "";
+        this.unicode = this.unicode.concat(toUnicodeColor(color));
+        this.unicode = this.unicode.concat(toUnicodeNumber(n));
+        this.unicode = this.unicode.concat((char) 27 + "[30m ");
+    }
+
+    private String toUnicodeColor(String color){
+        switch (color.toLowerCase()){
+            case "red":         return (char) 27 + "[31m ";
+            case "yellow":      return (char) 27 + "[33m ";
+            case "green":       return (char) 27 + "[32m ";
+            case "blue":        return (char) 27 + "[34m ";
+            case "purple":      return (char) 27 + "[35m ";
+            default: return "";
+        }
+    }
+
+    private String toUnicodeNumber(int number){
+        switch (number){
+            case 1:    return "\u2680 ";
+            case 2:    return "\u2681 ";
+            case 3:    return "\u2682 ";
+            case 4:    return "\u2683 ";
+            case 5:    return "\u2684 ";
+            case 6:    return "\u2685 ";
+            default: return "";
+        }
+    }
 
     public String toString(){
-        String string ="**  ";
-        string += this.number;
-        string += " ";
-        string += this.color;
-        string += " ";
-        string += this.location;
-        string += "  **";
-        return string;
+        return this.unicode;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -70,7 +94,6 @@ public class Dice {
 
     @Override
     public int hashCode() {
-
         return Objects.hash(number, color, location);
     }
 }
