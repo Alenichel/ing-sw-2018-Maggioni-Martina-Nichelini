9196db5b9f5e24b674b909a6973853a652da82b5
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-19 20:50:02 +0200

    Added main of server and client

--- src/main/java/it/polimi/se2018/AppClient.java
+++ src/main/java/it/polimi/se2018/AppClient.java
@@ -5,11 +5,21 @@ import it.polimi.se2018.model.Player;
 import it.polimi.se2018.network.SocketClient;
 import it.polimi.se2018.view.CliView;
 
+import java.util.Scanner;
+
 public class AppClient {
 
     public static void main(String[] args) {
-        Player p = new Player("Alenichel");
-        CliView cw = new CliView(p);
+        System.out.println("[*] Please insert your unsername");
+        Scanner sinput = new Scanner(System.in);
+        String input = sinput.nextLine();
+
+        Player p = new Player(input.toString());
+
+        System.out.println("[*] " + p.getNickname() + " welcome to Sagrada");
+
+        CliView cw = new CliView(input.toString());
+
         SocketClient sc = new SocketClient("localhost", 9091, cw);
         cw.addObserver(sc);
         cw.run();
