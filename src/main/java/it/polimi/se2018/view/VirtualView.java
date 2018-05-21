fa1e43fee94439adbda640c494f3998610f320e3
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-21 15:35:41 +0200

    Name fixed

--- src/main/java/it/polimi/se2018/view/VirtualView.java
+++ src/main/java/it/polimi/se2018/view/VirtualView.java
@@ -11,10 +11,10 @@ import java.util.Observer;
 
 public class VirtualView extends View implements Observer {
 
-    private VirtualViewNetworkBridge virtualClient;
+    private VirtualViewNetworkBridge virtualViewNetworkBridge;
 
     public VirtualView (VirtualViewNetworkBridge virtualClient, Player player){
-        this.virtualClient = virtualClient;
+        this.virtualViewNetworkBridge = virtualClient;
         this.client = player;
         this.addObserver(ServerController.getInstance());
         Server.getInstance().addObserver(this);
@@ -26,12 +26,12 @@ public class VirtualView extends View implements Observer {
 
     //Da CONTROLLER a VIEW -> deve passare per la rete
     public void controllerCallback(Message callbackMessage){
-        virtualClient.controllerCallback(callbackMessage);
+        virtualViewNetworkBridge.controllerCallback(callbackMessage);
     }
 
     //Da MODEL a VIEW -> deve passare per la rete
     public void update(Observable o, Object msg){
-        virtualClient.update(o, msg);
+        virtualViewNetworkBridge.update(o, msg);
     }
 
 }
