5137995c17a1f37f15ba1f3a96c7f48ae8987e18
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-23 17:22:23 +0200

    Created enumeration for windows pattern card name

---
+++ src/main/java/it/polimi/se2018/utils/WindowPatternCardsName.java
@@ -0,0 +1,28 @@
+package it.polimi.se2018.utils;
+
+public enum WindowPatternCardsName {
+        virtus,
+        viaLux,
+        bellesguard,
+        keleidoscopicDream,
+        auroraeMagnificus,
+        sunCatcher,
+        shadowThief,
+        auroraSagradis,
+        firmitas,
+        batllo,
+        industria,
+        symhonyOfLight,
+        chromaticSplendor,
+        comitas,
+        firelight,
+        fractalDrops,
+        fulgorDelCielo,
+        gravitas,
+        luxAstram,
+        luxMundi,
+        luzCelestial,
+        ripplesOfLight,
+        sunsGlory,
+        waterOfLife
+}
