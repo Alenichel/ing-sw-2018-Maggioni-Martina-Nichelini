30fa22e3b41549a09d15c55e178b2005bcb95e58
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-23 13:24:31 +0200

    Simplified update method and replaced System.out calls with Logger's ones

--- src/main/java/it/polimi/se2018/controller/ServerController.java
+++ src/main/java/it/polimi/se2018/controller/ServerController.java
@@ -3,6 +3,7 @@ package it.polimi.se2018.controller;
 import it.polimi.se2018.exception.GameException;
 import it.polimi.se2018.message.*;
 import it.polimi.se2018.model.Game;
+import it.polimi.se2018.utils.Logger;
 import it.polimi.se2018.view.*;
 import it.polimi.se2018.model.Player;
 import it.polimi.se2018.model.Server;
@@ -14,6 +15,7 @@ import java.util.Observer;
 public class ServerController implements Observer{
     private static ServerController instance = null;
     private Server server = Server.getInstance();
+    private ArrayList<GameController> gameControllers;
 
     private Player getPlayerFromNick(String nickname){
         for (Player p : server.getOnlinePlayers()){
@@ -50,8 +52,8 @@ public class ServerController implements Observer{
      * Connect player to server and to first avaiable game
      * @param player
      */
-    private void connectPlayer (Player player, Observer o) {
-        server.addPlayer(server.getOnlinePlayers(), player); //add player to the list of online players
+    private synchronized void connectPlayer (Player player, Observer o) {
+        server.addPlayerToOnlinePlayers(player); //add player to the list of online players
         player.setOnline(true); //set player status to online
         try {
             if (this.server.getCurrentGame() == null) {
@@ -64,46 +66,56 @@ public class ServerController implements Observer{
             this.server.addPlayer(server.getInGamePlayers(), player); //add him to the list of ingame players
         }catch (IndexOutOfBoundsException e){
             this.server.addPlayer(server.getWaitingPlayers(), player); //in case of game full and not started, put it in waiting players
-        } catch (GameException e){ System.out.println(e);}
+        } catch (GameException e){ Logger.ERROR(e.toString());}
     }
 
     /**
      * Disconnect player to server
      * @param player
      */
-    private void disconnectPlayer (Player player) {
-        server.removePlayer(server.getOnlinePlayers(), player);
+    private synchronized void disconnectPlayer (Player player) {
+        server.removePlayerFromOnlinePlayers(player);
         player.setOnline(false);
+    }
+
+    private void handleConnectionMessage(Observable observable, ConnectionMessage message){
+        if (message.isConnecting()){
+            if ( message.getTarget() == null  )
+                this.connectPlayer(message.getRequester(), (Observer)observable);
+        }
+        else {
+            if ( message.getTarget() == null  ) {
+                // delete the VirtualView as observer from Server and from all games.
+                server.deleteObserver((VirtualView) observable);
+                for (Game g : this.server.getActiveGames()) g.deleteObserver((VirtualView)observable);
+                this.disconnectPlayer(message.getRequester());
+            }
+        }
+    }
+
+    private void handleRequestMessage(Observable observable, RequestMessage rMsg){
+        if (rMsg.getRequest().equals("SubscribePlayer")) {
+            this.getPlayerFromNick(((View) observable).getClient().getNickname()).addObserver((Observer) observable);
+        }
+
+        else if (rMsg.getRequest().equals("ConnectedPlayers")){
+            ((VirtualView) observable).controllerCallback(new GiveMessage("ConnectedPlayers", server.getOnlinePlayers()));
+        }
     }
 
     public void update (Observable observable, Object message){
 
-        System.out.println(((Message)message).getMessageType());
+        Logger.NOTIFICATION(":SERVER_CONTROLLER: Receveid -> " + ((Message) message).getMessageType());
         switch(((Message)message).getMessageType()){
 
             case "ConnectionMessage":
-                if (((ConnectionMessage)message).isConnecting()){
-                    if ( ((ConnectionMessage)message).getTarget() == null  )
-                        this.connectPlayer(((ConnectionMessage)message).getRequester(), (Observer)observable);
-                }
-                else {
-                    if ( ((ConnectionMessage)message).getTarget() == null  )
-                        this.disconnectPlayer(((ConnectionMessage)message).getRequester());
-                }
+               handleConnectionMessage(observable, (ConnectionMessage)message);
             break;
 
 
             case "RequestMessage":
-                RequestMessage rMsg = ((RequestMessage)message);
+                handleRequestMessage(observable, (RequestMessage)message);
 
-                if (rMsg.getRequest().equals("SubscribePlayer")) {
-                    this.getPlayerFromNick(((View) observable).getClient().getNickname()).addObserver((Observer) observable);
-                    System.out.println("iscrittoAlPlayer");
-                }
-
-                else if (rMsg.getRequest().equals("ConnectedPlayers")){
-                    ((VirtualView) observable).controllerCallback(new GiveMessage("ConnectedPlayers", server.getOnlinePlayers()));
-                }
             break;
 
             default:
