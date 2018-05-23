3c53b2c3989dbfd8e6c7d6511ac43f04022b1c87
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-23 17:38:01 +0200

    Added Lens Cutter constructor

--- src/main/java/it/polimi/se2018/strategy/toolcard/LensCutter.java
+++ src/main/java/it/polimi/se2018/strategy/toolcard/LensCutter.java
@@ -1,8 +1,5 @@
 package it.polimi.se2018.strategy.toolcard;
 
-import it.polimi.se2018.Dice;
-import it.polimi.se2018.ToolCardEffectStrategy;
-import it.polimi.se2018.WindowCell;
 import it.polimi.se2018.exception.NotEmptyWindowCellException;
 import it.polimi.se2018.model.Dice;
 import it.polimi.se2018.model.ToolCardEffectStrategy;
