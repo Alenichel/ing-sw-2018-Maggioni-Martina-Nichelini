a258d7e94a8075aba7eff4eb6d0b71357773aba2
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-23 17:23:13 +0200

    Added toolCards and pattern cards initialization

--- src/main/java/it/polimi/se2018/controller/GameSetupController.java
+++ src/main/java/it/polimi/se2018/controller/GameSetupController.java
@@ -1,57 +1,69 @@
 package it.polimi.se2018.controller;
 
-import it.polimi.se2018.model.Dice;
-import it.polimi.se2018.model.Game;
-import it.polimi.se2018.model.ToolCard;
-import it.polimi.se2018.model.Player;
-import it.polimi.se2018.model.WindowPatternCard;
+import it.polimi.se2018.model.*;
+import it.polimi.se2018.strategy.toolcard.*;
 import it.polimi.se2018.utils.DiceColor;
 import it.polimi.se2018.utils.DiceLocation;
+import it.polimi.se2018.utils.ToolCardsName;
+import it.polimi.se2018.utils.WindowPatternCardsName;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameSetupController implements Serializable {
 
     private Game associatedGame;
 
-    GameSetupController(Game game){
+    public GameSetupController(Game game){
         this.associatedGame = game;
     }
 
-
+    /**
+     * This method initialize all windows pattern cards, before asking player wich card he wants to use during this game
+     */
     private void initializePatternCard(){
-        ArrayList<String> windowsPatternCardsName = new ArrayList<>();
-
-        windowsPatternCardsName.add("virtus");
-        windowsPatternCardsName.add("viaLux");
-        windowsPatternCardsName.add("bellesguard");
-        windowsPatternCardsName.add("keleidoscopicDream");
-        windowsPatternCardsName.add("auroraeMagnificus");
-        windowsPatternCardsName.add("sunCatcher");
-        windowsPatternCardsName.add("shadowThief");
-        windowsPatternCardsName.add("auroraSagradis");
-        windowsPatternCardsName.add("firmitas");
-        windowsPatternCardsName.add("batllo");
-        windowsPatternCardsName.add("industria");
-        windowsPatternCardsName.add("symhonyOfLight");
-        windowsPatternCardsName.add("chromaticSplendor");
-        windowsPatternCardsName.add("comitas");
-        windowsPatternCardsName.add("firelight");
-        windowsPatternCardsName.add("fractalDrops");
-        windowsPatternCardsName.add("fulgorDelCielo");
-        windowsPatternCardsName.add("gravitas");
-        windowsPatternCardsName.add("luxAstram");
-        windowsPatternCardsName.add("luxMundi");
-        windowsPatternCardsName.add("luzCelestial");
-        windowsPatternCardsName.add("ripplesOfLight");
-        windowsPatternCardsName.add("sunsGlory");
-        windowsPatternCardsName.add("waterOfLife");
-
-        for (String str : windowsPatternCardsName){
-            this.associatedGame.getPatternCards().add(new WindowPatternCard(str));
+        for(WindowPatternCardsName w : WindowPatternCardsName.values()){
+            this.associatedGame.getPatternCards().add(new WindowPatternCard(w.toString()));
         }
     }
+
+    private void initializeToolCards(){
+        int n;
+        ArrayList<String> toolName = new ArrayList<>();
+        ArrayList<ToolCard> selectedToolCards = new ArrayList<ToolCard>();
+        for(ToolCardsName t : ToolCardsName.values()){
+            toolName.add(t.toString());
+        }
+
+        Random rand = new Random();
+        n = rand.nextInt(4)+1;
+
+        for(int i = 0; i<3; i++){
+            n = rand.nextInt(toolName.size());
+            selectedToolCards.add(new ToolCard(nameToObject(toolName.get(n))));
+            toolName.remove(n);
+        }
+    }
+
+    private ToolCardEffectStrategy nameToObject(String name){
+        switch (name){
+            case "GrozingPliers" : return new GrozingPliers();
+            case "EnglomiseBrush" : return new EnglomiseBrush();
+            case "CopperFoilBurnisher" : return new CopperFoilBurnisher();
+            case "Lathekin" : return new Lathekin();
+            case "LensCutter" : return new LensCutter();
+            case "FluxBrush" : return new FluxBrush();
+            case "GlazingHammer" : return new GlazingHammer();
+            case "RunningPliers" : return new RunningPliers();
+            case "CorkBackedStraightedge" : return new CorkBackedStraightedge();
+            case "GrindingStone" : return new GrindingStone();
+            case "FluxRemover" : return new FluxRemover();
+            case "TapWheel" : return new TapWheel();
+            default : throw new IllegalArgumentException();
+        }
+    }
+
 
     /**
      * This method initialize all dice that will be used during the game.
