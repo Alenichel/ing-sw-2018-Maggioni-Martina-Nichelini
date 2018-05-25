8553d763a7446d9a0c335aa3647d85fe102b4eea
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-25 17:16:46 +0200

    Created enum

---
+++ src/main/java/it/polimi/se2018/utils/ObjectiveCardsName.java
@@ -0,0 +1,14 @@
+package it.polimi.se2018.utils;
+
+public enum ObjectiveCardsName {
+    RowColorVariety,
+    ColumnColorVariety,
+    RowShadeVariety,
+    ColumnShadeVariety,
+    LightShades,
+    MediumShades,
+    DarkShades,
+    ShadeVariety,
+    ColorDiagonals,
+    ColorVariety
+}
