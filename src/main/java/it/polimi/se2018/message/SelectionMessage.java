989ece88f2dea8e9dd57dd8277a735891c33bd95
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-25 23:53:55 +0200

    Added param selected

--- src/main/java/it/polimi/se2018/message/SelectionMessage.java
+++ src/main/java/it/polimi/se2018/message/SelectionMessage.java
@@ -4,11 +4,20 @@ public class SelectionMessage extends Message{
 
     private Object chosenItem;
     private int fromWhom;
+    private String selected;
 
     public SelectionMessage(Object item, int playerNumber){
         this.chosenItem = item;
         this.messageType = "SelectionMessage";
         this.fromWhom =  playerNumber;
+    }
+
+    //aggiunto per mandare da cliview al server il messaggio di scelta della patternCard,
+    public SelectionMessage(Object item, int playerNumber, String selected){
+        this.chosenItem = item;
+        this.messageType = "SelectionMessage";
+        this.fromWhom =  playerNumber;
+        this.selected = selected;
     }
 
     public int getPlayerNumber(){
