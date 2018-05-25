500a416004db5a93bcb4a87319f66c87a0907ef6
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-25 22:38:00 +0200

    Adapted test to new insertDice signature

--- src/test/java/it/polimi/se2018/ObjectivesTest.java
+++ src/test/java/it/polimi/se2018/ObjectivesTest.java
@@ -21,20 +21,20 @@ public class ObjectivesTest {
         Dice d3 = new Dice("red");
         Dice d4 = new Dice("blue");
 
-        wpc.insertDice(d1, 0, 0, false,false);
-        wpc.insertDice(d2, 1,0,false,false);
-        wpc.insertDice(d3, 2,0,false,false);
-        wpc.insertDice(d4, 3,0,false,false);
+        wpc.insertDice(d1, 0, 0, false,false,false);
+        wpc.insertDice(d2, 1,0,false,false,false);
+        wpc.insertDice(d3, 2,0,false, false,false);
+        wpc.insertDice(d4, 3,0,false,false, false);
 
-        wpc.insertDice(d1, 0, 2, false,false);
-        wpc.insertDice(d2, 1,2,false,false);
-        wpc.insertDice(d3, 2,2,false,false);
-        wpc.insertDice(d4, 3,2,false,false);
+        wpc.insertDice(d1, 0, 2, false,false,false);
+        wpc.insertDice(d2, 1,2,false, false, false);
+        wpc.insertDice(d3, 2,2,false, false, false);
+        wpc.insertDice(d4, 3,2,false, false, false);
 
-        wpc.insertDice(d1, 0, 4, false,false);
-        wpc.insertDice(d1, 1,4,false,false);
-        wpc.insertDice(d3, 2,4,false,false);
-        wpc.insertDice(d4, 3,4,false,false);
+        wpc.insertDice(d1, 0, 4, false, false, false);
+        wpc.insertDice(d1, 1,4,false,false,false);
+        wpc.insertDice(d3, 2,4,false,false, false);
+        wpc.insertDice(d4, 3,4,false, false,false);
 
         ScorePointStrategy sps = new ColumnVariety(VarietyType.COLOR);
         Assert.assertEquals(10, sps.scorePoint(wpc));
@@ -50,23 +50,23 @@ public class ObjectivesTest {
         Dice d4 = new Dice("blue");
         Dice d5 = new Dice("green");
 
-        wpc.insertDice(d1, 0, 0, false,false);
-        wpc.insertDice(d2, 0,1,false,false);
-        wpc.insertDice(d3, 0,2,false,false);
-        wpc.insertDice(d4, 0,3,false,false);
-        wpc.insertDice(d5, 0,4,false,false);
+        wpc.insertDice(d1, 0, 0, false,false, false);
+        wpc.insertDice(d2, 0,1,false,false, false);
+        wpc.insertDice(d3, 0,2,false,false, false);
+        wpc.insertDice(d4, 0,3,false,false, false);
+        wpc.insertDice(d5, 0,4,false, false, false);
 
-        wpc.insertDice(d1, 1, 0, false,false);
-        wpc.insertDice(d2, 1,1,false,false);
-        wpc.insertDice(d3, 1,2,false,false);
-        wpc.insertDice(d4, 1,3,false,false);
-        wpc.insertDice(d5, 1,4,false,false);
+        wpc.insertDice(d1, 1, 0, false,false, false);
+        wpc.insertDice(d2, 1,1,false,false,false);
+        wpc.insertDice(d3, 1,2,false,false, false);
+        wpc.insertDice(d4, 1,3,false, false, false);
+        wpc.insertDice(d5, 1,4,false,false,false);
 
-        wpc.insertDice(d1, 2, 0, false,false);
-        wpc.insertDice(d1, 2,1,false,false);
-        wpc.insertDice(d3, 2,2,false,false);
-        wpc.insertDice(d4, 2,3,false,false);
-        wpc.insertDice(d5, 2,4,false,false);
+        wpc.insertDice(d1, 2, 0, false,false, false);
+        wpc.insertDice(d1, 2,1,false,false,false);
+        wpc.insertDice(d3, 2,2,false,false,false);
+        wpc.insertDice(d4, 2,3,false,false,false);
+        wpc.insertDice(d5, 2,4,false,false,false);
 
         ScorePointStrategy sps = new RowVariety(VarietyType.COLOR);
         Assert.assertEquals(12, sps.scorePoint(wpc));
@@ -83,14 +83,14 @@ public class ObjectivesTest {
         Dice d4 = new Dice("blue");
         Dice d5 = new Dice("green");
 
-        wpc.insertDice(d1, 0, 0, false,false);
-        wpc.insertDice(d2, 1,1,false,false);
-        wpc.insertDice(d3, 2,2,false,false);
-        wpc.insertDice(d4, 3,3,false,false);
-        wpc.insertDice(d5, 3,2,false,false);
+        wpc.insertDice(d1, 0, 0, false,false,false);
+        wpc.insertDice(d2, 1,1,false,false,false);
+        wpc.insertDice(d3, 2,2,false,false,false);
+        wpc.insertDice(d4, 3,3,false,false,false);
+        wpc.insertDice(d5, 3,2,false,false,false);
 
-        wpc.insertDice(d1, 3,4, false,false);
-        wpc.insertDice(d1, 2,1,false,false);
+        wpc.insertDice(d1, 3,4, false,false,false);
+        wpc.insertDice(d1, 2,1,false,false,false);
 
         ScorePointStrategy sps = new ColorVariety();
         Assert.assertEquals(4,sps.scorePoint(wpc));
@@ -108,15 +108,15 @@ public class ObjectivesTest {
         Dice d5 = new Dice("green",5);
         Dice d6 = new Dice("green", 6);
 
-        wpc.insertDice(d1, 0, 0, false,false);
-        wpc.insertDice(d2, 1,1,false,false);
-        wpc.insertDice(d3, 2,2,false,false);
-        wpc.insertDice(d4, 3,3,false,false);
-        wpc.insertDice(d5, 3,2,false,false);
-        wpc.insertDice(d6, 0,1,false,false);
+        wpc.insertDice(d1, 0, 0, false,false,false);
+        wpc.insertDice(d2, 1,1,false,false,false);
+        wpc.insertDice(d3, 2,2,false,false,false);
+        wpc.insertDice(d4, 3,3,false,false,false);
+        wpc.insertDice(d5, 3,2,false,false,false);
+        wpc.insertDice(d6, 0,1,false,false,false);
 
-        wpc.insertDice(d1, 3,4, false,false);
-        wpc.insertDice(d1, 2,1,false,false);
+        wpc.insertDice(d1, 3,4, false,false,false);
+        wpc.insertDice(d1, 2,1,false,false,false);
         ScorePointStrategy sps = new ShadeVariety();
         Assert.assertEquals(5,sps.scorePoint(wpc));
     }
@@ -131,13 +131,13 @@ public class ObjectivesTest {
         Dice d4 = new Dice("red",4);
         Dice d5 = new Dice("yellow", 5);
 
-        wpc.insertDice(d1, 0, 0, false,false);
-        wpc.insertDice(d2, 1,1,false,false);
-        wpc.insertDice(d3, 1,2,false,false);
-        wpc.insertDice(d4, 3,2,false,false);
-        wpc.insertDice(d5, 1,3,false,false);
+        wpc.insertDice(d1, 0, 0, false,false,false);
+        wpc.insertDice(d2, 1,1,false,false,false);
+        wpc.insertDice(d3, 1,2,false,false,false);
+        wpc.insertDice(d4, 3,2,false,false,false);
+        wpc.insertDice(d5, 1,3,false,false,false);
 
-        ScorePointStrategy sps = new Shades("medium", 3, 4);
+        ScorePointStrategy sps = new Shades("medium");
         Assert.assertEquals(4,sps.scorePoint(wpc));
     }
 
@@ -146,19 +146,22 @@ public class ObjectivesTest {
         WindowPatternCard wpc = new WindowPatternCard("auroraeMagnificus");
 
         Dice d1 = new Dice("yellow");
-        Dice d2 = new Dice("yellow");
-        Dice d3 = new Dice("blue");
-        Dice d4 = new Dice("blue");
-        Dice d5 = new Dice("blue");
+        Dice d2 = new Dice("blue");
 
-        wpc.insertDice(d1, 0, 0, false,false);
+        /*wpc.insertDice(d2, 0, 0, false,false);
+        wpc.insertDice(d2, 0, 2, false,false);
         wpc.insertDice(d2, 1,1,false,false);
-        wpc.insertDice(d3, 3,2,false,false);
-        wpc.insertDice(d4, 2,3,false,false);
-        wpc.insertDice(d5, 3,4,false,false);
+        wpc.insertDice(d2, 2,2,false,false);
+        wpc.insertDice(d2, 3,3,false,false);*/
+        wpc.insertDice(d1, 0,3,false,false,false);
+        wpc.insertDice(d1, 2,3,false,false,false);
+        wpc.insertDice(d1, 1,4,false,false,false);
+        wpc.insertDice(d1, 3,2,false,false,false);
+        wpc.insertDice(d1, 1,2,false,false,false);
 
+        System.out.println(wpc);
         ScorePointStrategy sps = new ColorDiagonals();
-        Assert.assertEquals(5,sps.scorePoint(wpc));
+        Assert.assertEquals(7,sps.scorePoint(wpc));
     }
 
     @Test
@@ -171,11 +174,11 @@ public class ObjectivesTest {
         Dice d4 = new Dice("blue");
         Dice d5 = new Dice("blue");
 
-        wpc.insertDice(d1, 0, 0, false,false);
-        wpc.insertDice(d2, 1,2,false,false);
-        wpc.insertDice(d3, 3,2,false,false);
-        wpc.insertDice(d4, 2,3,false,false);
-        wpc.insertDice(d5, 3,4,false,false);
+        wpc.insertDice(d1, 0, 0, false,false,false);
+        wpc.insertDice(d2, 1,2,false,false,false);
+        wpc.insertDice(d3, 3,2,false,false,false);
+        wpc.insertDice(d4, 2,3,false,false,false);
+        wpc.insertDice(d5, 3,4,false,false,false);
 
         PrivateShadesOfColor sp = new PrivateShadesOfColor("blue");
         Assert.assertEquals(3, sp.scorePoint(wpc));
