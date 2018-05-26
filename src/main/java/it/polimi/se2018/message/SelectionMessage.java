f05b3cd61d7c3b4622967762247432cea0ea1f46
Alessandro Nichelini <ale.nichelg@gmail.com>
2018-05-27 01:22:07 +0200

    Added getter and modified constructor param

--- src/main/java/it/polimi/se2018/message/SelectionMessage.java
+++ src/main/java/it/polimi/se2018/message/SelectionMessage.java
@@ -1,26 +1,32 @@
 package it.polimi.se2018.message;
 
+import it.polimi.se2018.model.Player;
+
 public class SelectionMessage extends Message{
 
     private Object chosenItem;
-    private int fromWhom;
+    private Player fromWhom;
     private String selected;
 
-    public SelectionMessage(Object item, int playerNumber){
+    public SelectionMessage(Object item, Player player){
         this.chosenItem = item;
         this.messageType = "SelectionMessage";
-        this.fromWhom =  playerNumber;
+        this.fromWhom =  player;
     }
 
     //aggiunto per mandare da cliview al server il messaggio di scelta della patternCard,
-    public SelectionMessage(Object item, int playerNumber, String selected){
+    public SelectionMessage(Object item, Player player, String selected){
         this.chosenItem = item;
         this.messageType = "SelectionMessage";
-        this.fromWhom =  playerNumber;
+        this.fromWhom =  player;
         this.selected = selected;
     }
 
-    public int getPlayerNumber(){
+    public String getSelected() {
+        return selected;
+    }
+
+    public Player getPlayer(){
         return this.fromWhom;
     }
 
