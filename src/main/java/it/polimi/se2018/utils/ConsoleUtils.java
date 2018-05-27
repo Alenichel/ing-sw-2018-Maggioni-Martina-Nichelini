fae2499f13d5b2dacf5e71fab86d3a9976457f4a
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-27 23:25:16 +0200

    Added method to print multiple object

---
+++ src/main/java/it/polimi/se2018/utils/ConsoleUtils.java
@@ -0,0 +1,28 @@
+package it.polimi.se2018.utils;
+
+        import it.polimi.se2018.model.WindowPatternCard;
+
+        import java.util.ArrayList;
+
+public class ConsoleUtils {
+    public void multiplePrint(ArrayList<WindowPatternCard> ws) {
+        ArrayList<String[]> tokensTOT = new ArrayList<>();
+
+        int i = 0, j = 0;
+        for (WindowPatternCard w : ws) {
+            tokensTOT.add(ws.get(i).toString().split("\n"));
+            i++;
+        }
+
+        for(int a = 0; a < tokensTOT.get(0).length; a++){
+            for(int b = 0; b < tokensTOT.size(); b++){
+                System.out.print(tokensTOT.get(b)[a] + "   ");
+                //Logger.log(LoggerType.CLIENT_SIDE, tokensTOT.get(b)[a]);
+            }
+            System.out.println();
+            //Logger.log(LoggerType.CLIENT_SIDE,"\n");
+        }
+    }
+}
+
+
