ccba13ec26a879027379d5e9f65ccbd623b3e27c
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-28 18:28:05 +0200

    Moved to ConsoleUtils method to convert Color and dice number in uncicode

--- src/main/java/it/polimi/se2018/utils/ConsoleUtils.java
+++ src/main/java/it/polimi/se2018/utils/ConsoleUtils.java
@@ -21,6 +21,29 @@ public class ConsoleUtils {
             System.out.println();
         }
     }
+
+    public String toUnicodeColor(String color){
+        switch (color.toLowerCase()){
+            case "red":         return (char) 27 + "[31m";
+            case "yellow":      return (char) 27 + "[33m";
+            case "green":       return (char) 27 + "[32m";
+            case "blue":        return (char) 27 + "[34m";
+            case "purple":      return (char) 27 + "[35m";
+            default: return "";
+        }
+    }
+
+    public String toUnicodeNumber(int number){
+        switch (number){
+            case 1:    return "\u2680";
+            case 2:    return "\u2681";
+            case 3:    return "\u2682";
+            case 4:    return "\u2683";
+            case 5:    return "\u2684";
+            case 6:    return "\u2685";
+            default: return "";
+        }
+    }
 }
 
 
