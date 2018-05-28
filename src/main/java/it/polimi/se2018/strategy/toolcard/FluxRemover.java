295e4f6b09a4323c630415ae9a470ba5954f4df3
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-28 19:03:59 +0200

    Added description and name

--- src/main/java/it/polimi/se2018/strategy/toolcard/FluxRemover.java
+++ src/main/java/it/polimi/se2018/strategy/toolcard/FluxRemover.java
@@ -1,6 +1,8 @@
 package it.polimi.se2018.strategy.toolcard;
 
+import it.polimi.se2018.model.ToolCard;
 import it.polimi.se2018.model.ToolCardEffectStrategy;
+import it.polimi.se2018.utils.ToolCardsName;
 
 import java.io.Serializable;
 
@@ -10,9 +12,12 @@ import java.io.Serializable;
  * The player gets to choose its number and places it on his window pattern card obeying all
  * restrictions or returns it to the draft pool.
  */
-public class FluxRemover implements ToolCardEffectStrategy, Serializable {
+public class FluxRemover extends ToolCard implements ToolCardEffectStrategy, Serializable {
 
-    public FluxRemover(){ }
+    public FluxRemover(){
+        this.setName(ToolCardsName.FluxBrush.toString());
+        this.setDescription("After drafting return the die to the dice bag and pull 1 die from the bag. Choose a value and place the new die, obeying all placement restriction or return to the dice bag ");
+    }
 
     public FluxRemover FluxRemover(FluxRemover fluxRemover) {
         return fluxRemover;
