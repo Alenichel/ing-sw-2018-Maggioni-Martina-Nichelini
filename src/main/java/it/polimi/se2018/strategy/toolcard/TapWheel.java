8eb46f59639019bcbda7ff806f2a3efe15f3f9be
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-28 19:18:36 +0200

    Added description and name

--- src/main/java/it/polimi/se2018/strategy/toolcard/TapWheel.java
+++ src/main/java/it/polimi/se2018/strategy/toolcard/TapWheel.java
@@ -1,6 +1,8 @@
 package it.polimi.se2018.strategy.toolcard;
 
+import it.polimi.se2018.model.ToolCard;
 import it.polimi.se2018.model.ToolCardEffectStrategy;
+import it.polimi.se2018.utils.ToolCardsName;
 
 import java.io.Serializable;
 
@@ -8,9 +10,12 @@ import java.io.Serializable;
  * This class implements Tool Card #12 "Tap Wheel" which lets the player move up to two dice of the
  * same color that match the color of a die on the Round Track obeying all restrictions.
  */
-public class TapWheel implements ToolCardEffectStrategy, Serializable {
+public class TapWheel extends ToolCard implements ToolCardEffectStrategy, Serializable {
 
-    public TapWheel(){ }
+    public TapWheel(){
+        this.setName(ToolCardsName.TapWheel.toString());
+        this.setDescription("Move up to two dice of the same color that match the color of a die in the round track. You must obey all the placement restriction");
+    }
 
     public TapWheel TapWheel(TapWheel tapWheel) {
         return tapWheel;
