98cd6c50955ea35a881f3c91afe3bd18a79b3c5f
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-28 19:10:01 +0200

    Added description and name

--- src/main/java/it/polimi/se2018/strategy/toolcard/GlazingHammer.java
+++ src/main/java/it/polimi/se2018/strategy/toolcard/GlazingHammer.java
@@ -1,7 +1,9 @@
 package it.polimi.se2018.strategy.toolcard;
 
 import it.polimi.se2018.model.Dice;
+import it.polimi.se2018.model.ToolCard;
 import it.polimi.se2018.model.ToolCardEffectStrategy;
+import it.polimi.se2018.utils.ToolCardsName;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -11,11 +13,14 @@ import java.util.ArrayList;
  * in the draft pool.
  * This may only be used on the second turn before drafting
  */
-public class GlazingHammer implements ToolCardEffectStrategy, Serializable {
+public class GlazingHammer extends ToolCard implements ToolCardEffectStrategy, Serializable {
 
     private ArrayList<Dice> draftedDice;
 
-    public GlazingHammer(){ }
+    public GlazingHammer(){
+        this.setName(ToolCardsName.GlazingHammer.toString());
+        this.setDescription("Re roll all dice in the drafted pool. This may only used on your second turn before drafting.");
+    }
 
     public GlazingHammer GlazingHammer(GlazingHammer glazingHammer, ArrayList<Dice> draftedDice){
         glazingHammer.draftedDice = draftedDice;
