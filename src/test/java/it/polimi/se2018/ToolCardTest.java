95f8adde1e5d56aa4b22bc6259fc52c1f36c42a8
Stefano Martina <stefano.martina@mail.polimi.it>
2018-05-31 09:54:48 +0200

    Added tests

--- src/test/java/it/polimi/se2018/ToolCardTest.java
+++ src/test/java/it/polimi/se2018/ToolCardTest.java
@@ -4,9 +4,7 @@ import it.polimi.se2018.exception.NotEmptyWindowCellException;
 import it.polimi.se2018.exception.NotValidInsertion;
 import it.polimi.se2018.exception.ToolCardException;
 import it.polimi.se2018.model.*;
-import it.polimi.se2018.strategy.toolcard.EnglomiseBrush;
-import it.polimi.se2018.strategy.toolcard.GrozingPliers;
-import it.polimi.se2018.strategy.toolcard.Lathekin;
+import it.polimi.se2018.strategy.toolcard.*;
 import it.polimi.se2018.utils.DiceColor;
 import it.polimi.se2018.utils.WindowPatternCardsName;
 import static org.junit.Assert.*;
@@ -14,6 +12,7 @@ import it.polimi.se2018.strategy.toolcard.Lathekin;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.spec.ECField;
 import java.util.ArrayList;
 
 public class ToolCardTest {
@@ -53,8 +52,8 @@ public class ToolCardTest {
 
         Dice diceStart1 = new Dice(DiceColor.green);
         Dice diceStart2 = new Dice(DiceColor.purple);
-        System.out.println(diceStart1.toString());
-        System.out.println(diceStart2.toString());
+        //System.out.println(diceStart1.toString());
+        //System.out.println(diceStart2.toString());
         try {
             wstart1.setAssignedDice(diceStart1);
             wstart2.setAssignedDice(diceStart2);
@@ -64,7 +63,7 @@ public class ToolCardTest {
         }
         Lathekin lathekin = new Lathekin();
         ToolCard toolCardProvaLathekin = new ToolCard(lathekin);
-        System.out.println(windowPatternCard.toString());
+        //System.out.println(windowPatternCard.toString());
         lathekin = lathekin.Lathekin(lathekin, windowPatternCard, wstart1, wstart2, wend1, wend2);
         try {
             lathekin.executeEffect();
@@ -80,30 +79,77 @@ public class ToolCardTest {
     }
 
     @Test
-    public void testCorkEnglomiseBrush() {
+    public void testEnglomiseBrush() {
         Dice d = new Dice(DiceColor.purple);
         EnglomiseBrush englomiseBrush = new EnglomiseBrush();
         WindowPatternCard w = new WindowPatternCard(WindowPatternCardsName.auroraeMagnificus);
         englomiseBrush = englomiseBrush.EnglomiseBrush(englomiseBrush, w, w.getCell(1, 1), w.getCell(0, 1));
+        //0,1-->1,2
+        //System.out.println(w.toString());
 
         try {
-            w.insertDice(d, 1, 1, true, true, true);
+            w.insertDice(d, 1, 1, true, true, false);
         } catch (NotValidInsertion | NotEmptyWindowCellException e) {
+            System.out.println(e+"1--");
             fail();
         }
+        //System.out.println(w.toString());
 
-        System.out.println(w.toString());
 
         try {
             englomiseBrush.executeEffect();
         } catch (ToolCardException e) {
-            System.out.println(e);
+            System.out.println(e+"2--");
             fail();
         } catch (NotEmptyWindowCellException e) {
             System.out.println(e);
             fail();
         }
+
+        System.out.println(w.toString());
+
         Assert.assertEquals(w.getGrid()[0][1].getAssignedDice(), d);
         Assert.assertEquals(w.getGrid()[1][1].getAssignedDice(), null);
+    }
+
+    @Test
+    public void testGrozingPliers(){
+        Dice d = new Dice(DiceColor.red);
+        GrozingPliers grozingPliers = new GrozingPliers();
+        grozingPliers.GrozingPliers(grozingPliers,d, true);
+        int n = d.getNumber();
+        grozingPliers.executeEffect();
+
+        if (n != 6)
+            Assert.assertEquals(n+1, d.getNumber());
+        else
+            Assert.assertEquals(n, d.getNumber());
+
+    }
+
+    @Test
+    public void testCopperFoilBurnisher(){
+        WindowPatternCard w = new WindowPatternCard(WindowPatternCardsName.auroraeMagnificus);
+        WindowCell s = w.getCell(1,1);
+        WindowCell e = w.getCell(0,4);
+        Dice d = new Dice(DiceColor.red);
+        CopperFoilBurnisher copperFoilBurnisher = new CopperFoilBurnisher();
+        copperFoilBurnisher.CopperFoilBurnisher(copperFoilBurnisher, w, s, e);
+        //System.out.println(w.toString());
+
+        try {
+            w.insertDice(d, s.getRow(), s.getColumn(), true, true, false);
+        }catch (Exception p){
+            System.out.println(p+"1---");
+        }
+        try {
+            copperFoilBurnisher.executeEffect();
+        }catch (Exception p){
+            System.out.println(p+"2 ---");
+        }
+
+        //System.out.println(w.toString());
+
+
     }
 }
