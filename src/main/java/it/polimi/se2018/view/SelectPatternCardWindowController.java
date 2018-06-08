29a3aeab179fdc25e6d36101e9d03e552349f117
Stefano Martina <stefano.martina@mail.polimi.it>
2018-06-08 21:58:50 +0200

    Added patternCard selection

--- src/main/java/it/polimi/se2018/view/SelectPatternCardWindowController.java
+++ src/main/java/it/polimi/se2018/view/SelectPatternCardWindowController.java
@@ -1,32 +1,43 @@
 package it.polimi.se2018.view;
 
+import it.polimi.se2018.message.SelectionMessage;
 import it.polimi.se2018.model.Player;
 import it.polimi.se2018.model.WindowPatternCard;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class SelectPatternCardWindowController {
 
-    protected void printPool(Stage primaryStage, Player player){
+public class SelectPatternCardWindowController implements Serializable {
+    private Player player;
+    private GuiView gw;
+    @FXML private transient ImageView window1;
+    @FXML private transient ImageView window2;
+    @FXML private transient ImageView window3;
+    @FXML private transient ImageView window4;
+
+
+    protected void printPool(Stage primaryStage, Player player, GuiView gw){
+        this.gw = gw;
+        this.player = player;
         Scene scene = primaryStage.getScene();
         ArrayList<ImageView> imageViews = new ArrayList<>();
-        imageViews.add((ImageView) scene.lookup("#window1"));
-        imageViews.add((ImageView) scene.lookup("#window2"));
-        imageViews.add((ImageView) scene.lookup("#window3"));
-        imageViews.add((ImageView) scene.lookup("#window4"));
+        imageViews.add(window1);
+        imageViews.add(window2);
+        imageViews.add(window3);
+        imageViews.add(window4);
 
         ArrayList<Image> images = new ArrayList<>();
 
@@ -43,8 +54,23 @@ public class SelectPatternCardWindowController {
             imageView.setImage(images.get(i));
             i++;
         }
+
+        window1.setOnMouseClicked((MouseEvent e) -> {
+            handleClick(1);
+        });
+        window2.setOnMouseClicked((MouseEvent e) -> {
+            handleClick(2);
+        });
+        window3.setOnMouseClicked((MouseEvent e) -> {
+            handleClick(3);
+        });
+        window4.setOnMouseClicked((MouseEvent e) -> {
+            handleClick(4);
+        });
     }
 
-
+    private void handleClick(int n){
+        gw.selectedPatternCard(n);
+    }
 
 }
