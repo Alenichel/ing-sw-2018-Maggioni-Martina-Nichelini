815b0bb1e7d719878a7267f2b31abd171582c3e3
Stefano Martina <stefano.martina@mail.polimi.it>
2018-06-08 22:01:02 +0200

    Created controller for FXML Game Window

---
+++ src/main/java/it/polimi/se2018/view/GameWindowController.java
@@ -0,0 +1,34 @@
+package it.polimi.se2018.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class GameWindowController {
+
+    @FXML private transient GridPane windowPattern0;
+
+    @FXML private transient Label name1;
+
+    @FXML private transient Label name3;
+
+    @FXML private transient Label name2;
+
+    @FXML private transient Label favToken;
+
+    @FXML private transient GridPane windowPattern11;
+
+    @FXML private transient GridPane windowPattern111;
+
+    @FXML private transient GridPane windowPattern1111;
+
+    @FXML private transient Pane privateObjective;
+
+    @FXML private transient Pane objective1;
+
+    @FXML private transient Pane objective3;
+
+    @FXML private transient Pane objective2;
+
+}
