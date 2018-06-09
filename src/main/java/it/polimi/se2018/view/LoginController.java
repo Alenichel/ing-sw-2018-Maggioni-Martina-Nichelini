edb2352c117024a03ba0828d993359682ba5bd6d
Stefano Martina <stefano.martina@mail.polimi.it>
2018-06-09 16:41:48 +0200

    Fixed problems

--- src/main/java/it/polimi/se2018/view/LoginController.java
+++ src/main/java/it/polimi/se2018/view/LoginController.java
@@ -1,23 +1,23 @@
 package it.polimi.se2018.view;
 
 import it.polimi.se2018.model.Server;
+import it.polimi.se2018.network.RMIClient;
 import it.polimi.se2018.network.SocketClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.Serializable;
 import java.util.ResourceBundle;
-
+import javafx.scene.image.Image;
 public class LoginController implements Serializable{
 
     @FXML private transient Button button;
-
     @FXML private transient TextField username;
-
     @FXML private transient TextField password;
 
 
@@ -39,10 +39,19 @@ public class LoginController implements Serializable{
         if (!(Server.getInstance().isConfigurationRequired())) {
             logInPassword = null;
         }
+
+        //String methodConnection = toggleConnection.getSelectedToggle().getUserData().toString();
+        /*if(methodConnection.equals("rmi")){
+            //RMI CONNECTION
+            RMIClient rmiClient = new RMIClient();
+            gw.addObserver(rmiClient.run(gw, logInUsernme, logInPassword));
+        }else {
+            //SOCKET CONNECTION
+            SocketClient sc = new SocketClient("localhost", 9091, logInUsernme, logInPassword, gw);
+            gw.addObserver(sc);
+        }*/
         SocketClient sc = new SocketClient("localhost", 9091, logInUsernme, logInPassword, gw);
         gw.addObserver(sc);
-
-
         gw.run((Stage)button.getScene().getWindow());
     }
 }
